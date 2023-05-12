17bad25a197d425c3fdcab6ec5ac02dc80685311
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-12 12:10:49 -0700

    matrix multiplication

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -3,18 +3,15 @@
 
 
 def mat_mul(mat1, mat2):
-    # Get the dimensions of the matrices
+    """Get the dimensions of the matrices"""
     rows1, cols1 = len(mat1), len(mat1[0])
     rows2, cols2 = len(mat2), len(mat2[0])
 
-    # Check if the matrices can be multiplied
     if cols1 != rows2:
         return None
 
-    # Create a new matrix to store the result
     result = [[0 for _ in range(cols2)] for _ in range(rows1)]
 
-    # Perform matrix multiplication
     for i in range(rows1):
         for j in range(cols2):
             for k in range(rows2):
