46b77bbf7e519197a7a5a110d8862b1e33e9bf61
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-12 11:32:36 -0700

    ranspose 2d matrix

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python 3
+"""code to transpose a matrix"""
+
+
+def matrix_transpose(matrix):
+    """return transpose of a 2D matrix"""
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transpose = [[0 for j in range (rows)] for i in range(cols)]
+    for i in range(rows):
+        for j in range(cols):
+            transpose[j][i] = matrix[i][j]
+
+return transpose
