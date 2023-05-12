eb0ea540e6fa09879c45a99fc21c1dbe1391350e
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-12 11:41:17 -0700

    adding arrays

---
+++ math/linear_algebra/4-line_up.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""function to add 2 arrays"""
+
+
+def add_arrays(arr1, arr2):\
+    """adding arrays"""
+    if len(arr1) != len(arr2):
+        return None
+    result = []
+    for i in range(len(arr1)):
+        result.append(arr1[i] + arr2[i])
+    return result
