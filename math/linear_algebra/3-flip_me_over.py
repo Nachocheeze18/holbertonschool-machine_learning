4264e707a95b07c5a56106e9b24cbee8aaca4be0
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-12 11:36:19 -0700

    pycode style error

--- math/linear_algebra/3-flip_me_over.py
+++ math/linear_algebra/3-flip_me_over.py
@@ -6,7 +6,7 @@ def matrix_transpose(matrix):
     """return transpose of a 2D matrix"""
     rows = len(matrix)
     cols = len(matrix[0])
-    transpose = [[0 for j in range (rows)] for i in range(cols)]
+    transpose = [[0 for j in range(rows)] for i in range(cols)]
     for i in range(rows):
         for j in range(cols):
             transpose[j][i] = matrix[i][j]
