0b2c2f98495658b1758d983f7dede172db8f8487
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-11 23:05:49 -0700

    code that calculates the shape of a matrix

---
+++ math/linear_algebra/2-size_me_please.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+
+def matrix_transpose(matrix):
+"""calculate the shape of a matrix"""
+    shape []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
