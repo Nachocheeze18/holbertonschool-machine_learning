8c7ba3667a025f71960324f8acd11b689f025568
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-17 20:02:56 -0700

    trying to fix 9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -5,15 +5,6 @@
 def summation_i_squared(n):
     """Addition With Sigma"""
     if n >= 1:
-        return recursive_summation(n, 1, 0)
+        return int((n * (n + 1) * (2 * n + 1)) / 6)
     else:
         return None
-
-
-def recursive_summation(n, x, i):
-    """recursive summation function"""
-    if x <= n:
-        i = i + x ** 2
-        return recursive_summation(n, x + 1, i)
-    else:
-        return i
