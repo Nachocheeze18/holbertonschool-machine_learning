4279b8eae582131eeda688ef19ee3b9488a2b44d
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-24 13:26:19 -0700

    poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -2,6 +2,9 @@
 """Poisson"""
 
 
+e = 2.7182818285
+pi = 3.1415926536
+
 class Poisson:
     """Poisson Class"""
     def __init__(self, data=None, lambtha=1.):
@@ -19,16 +22,15 @@ class Poisson:
             self.lambtha = sum(data) / len(data)
 
     def pmf(self, k):
+"""pmf function"""
+        k = int(k)        
+        def factorial(n):
+            result = 1
+            for i in range(1, n + 1):
+            result *= i
+            return result
 
-        """convert k to an interger if not already"""
-        k = int(k)
-
-        """Check if k is out of range"""
-        if k < 0 or k >= len(self.data):
-            return 0
-
-        """Calculate the PMF value"""
-        total_count = sum(self.data)
-        pmf_value = self.data[k] / total_count
-
-        return pmf_value
+        top = e**(-self.lambtha) * self.lambtha**(k)
+        bot = factorial(self.k)
+        sum = top / bot
+    
