393ddf4da8391f1aa0f1f3b88bf1805e6454fe27
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-24 10:56:22 -0700

    poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -3,16 +3,17 @@
 
 
 class Poisson:
-"""Poisson Class"""
-def __init__(self, data=None, lambtha=1.):
-"""cunstructs the class"""
+    """Poisson Class"""
+    def __init__(self, data=None, lambtha=1.):
+        """Constructs the value"""
         if data is None:
             if lambtha <= 0:
-                raise ValueError("lambtha must be a positive value")
-            self.lambtha = float(lambtha)
+                raise ValueError('lambtha must be a positive value')
+            else:
+                self.lambtha = float(lambtha)
         else:
             if not isinstance(data, list):
-                raise TypeError("data must be a list")
+                raise TypeError('data must be a list')
             if len(data) < 2:
-                raise ValueError("data must contain multiple values")
-            self.lambtha = float(sum(data) / len(data))
+                raise ValueError('data must contain multiple values')
+            self.lambtha = sum(data) / len(data)
