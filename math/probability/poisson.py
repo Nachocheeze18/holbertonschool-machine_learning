ac4ba8b6b4918904506826995d3fe0c3b4db5f9d
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-24 13:04:38 -0700

    poisson.py

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -18,16 +18,16 @@ class Poisson:
                 raise ValueError('data must contain multiple values')
             self.lambtha = sum(data) / len(data)
     def pmf(self, k):
+        
         """convert k to an interger if not already"""
         k = int(k)
-        
+
         """Check if k is out of range"""
         if k < 0 or k >= len(self.data):
             return 0
-        
+
         """Calculate the PMF value"""
         total_count = sum(self.data)
         pmf_value = self.data[k] / total_count
-        
+
         return pmf_value
-    
