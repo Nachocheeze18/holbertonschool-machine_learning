8ca529b365c441a749504b7fc67a8547c2e35f89
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-24 20:35:48 -0700

    binomial.py

--- math/probability/binomial.py
+++ math/probability/binomial.py
@@ -8,13 +8,13 @@ class Binomial:
     """binomial class"""
     def __init__(self, data=None, n=1, p=0.5):
         """constructer"""
+        self.n = int(n)
+        self.p = float(p)
         if data is None:
             if n <= 0:
                 raise ValueError("n must be a positive value")
             if p <= 0 or p >= 1:
                 raise ValueError("p must be greater than 0 and less than 1")
-            self.n = int(n)
-            self.p = float(p)
         else:
             # Estimate n and p from the data
             self.n = len(data)
@@ -53,7 +53,7 @@ class Binomial:
         return result
 
     def cdf(self, k):
-        """cdf calculations"""
+        """cdf calculation"""
         if k < 0:
             return 0
 
