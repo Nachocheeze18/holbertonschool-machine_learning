7856ed7ae89eb545d5d625701150551aee150e37
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-24 17:34:39 -0700

    exponential.py, adding two functions

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -24,3 +24,38 @@ class Normal:
                 raise ValueError('data must contain multiple values')
 
             self.mean = sum(data) / len(data)
+
+            n = 0
+            for x in data:
+                n += (x - self.mean) ** 2
+            self.stddev = ((n / len(data)) ** 0.5)
+
+    def z_score(self, x):
+        """z_score"""
+        return ((x - self.mean) / self.stddev)
+
+    def x_value(self, z):
+        """x_value"""
+        return ((self.stddev * z) + self.mean)
+
+    def pdf(self, x):
+        """pdf calculations"""
+
+        b = -((x - self.mean) ** 2) / (2 * (self.stddev ** 2))
+        return (1 / (self.stddev * ((2 * pi) ** 0.5))) * (e ** (b))
+
+    def cdf(self, x):
+        """cdf calculations"""
+
+        z = self.z_score(x)
+        return 0.5 * (1 + self.erf(z / 2 ** .5))
+
+    def erf(self, x):
+        """erf function helper"""
+        a = (2 / (pi ** 0.5))
+        b = ((x ** 3) / 3)
+        c = ((x ** 5) / 10)
+        d = ((x ** 7) / 42)
+        e = ((x ** 9) / 216)
+
+        return a * (x - b + c - d + e)
