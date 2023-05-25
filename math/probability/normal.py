caabbf00aeb92153b6b73ce4135cff4592b47268
Nachocheeze18 <nachovelez490@gmail.com>
2023-05-24 17:18:15 -0700

    normal.py, constructing a class

---
+++ math/probability/normal.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Normal"""
+
+e = 2.7182818285
+pi = 3.1415926536
+
+
+class Normal:
+    """Normal Class"""
+    def __init__(self, data=None, mean=0., stddev=1.):
+        """Constructor"""
+        self.mean = float(mean)
+        self.stddev = float(stddev)
+
+        if data is None:
+            if stddev <= 0:
+                raise ValueError('stddev must be a positive value')
+            else:
+                self.lambtha = float(stddev)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            if len(data) < 2:
+                raise ValueError('data must contain multiple values')
+
+            self.mean = sum(data) / len(data)
