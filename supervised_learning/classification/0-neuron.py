17bce9bf7c207787f69609f1edebce373855937a
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-02 19:34:08 -0500

    problem with weight

--- supervised_learning/classification/0-neuron.py
+++ supervised_learning/classification/0-neuron.py
@@ -12,6 +12,6 @@ class Neuron:
         if nx < 1:
             raise ValueError("nx must be a positive integer")
 
-        self.W = np.random.normal(1, nx)
+        self.W = np.random.randn(1, nx)
         self.b = 0
         self.A = 0
