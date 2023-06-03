100226c0aaf84d37782dee778c49dccea82f43f0
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-03 13:59:30 -0500

    3-neuron.py, model cost\

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""nueron"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+    def __init__(self, nx):
+        """Cunstructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """Forward Propogation"""
+        self.__A = self.sigmoid(np.matmul(self.__W, X) + self.__b)
+        return self.__A
+
+    def sigmoid(self, X):
+        """Sigmoid"""
+        return 1 / (1 + np.exp(-X))
+
+    def cost(self, Y, A):
+        """model cost"""
+        m = Y.shape[1]
+        cost = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        return cost
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
