a2d04bae3b5bde165d2a735c91353c9cdc72a77d
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-03 13:29:11 -0500

    2-nueron.py, forward probagation and sigmoid function

---
+++ supervised_learning/classification/2-nueron.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""nueron"""
+
+
+import numpy as np
+
+class Nueron:
+    """Nueron Class"""
+    def __init__(self, nx):
+        """Cunstructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """Forward Propogation"""
+        self.__A = self.sigmoid(np.matmul(self.__W, X) + self.__b)
+        return self.__A
+
+    def sigmoid(self, X):
+        """Sigmoid"""
+        return 1 / (1 + np.exp(-X))
+
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
