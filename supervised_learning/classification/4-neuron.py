85269a0950671e57e13679ebfc2ccaa991c48626
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-04 16:48:38 -0500

    4-neuron.py, prediction

---
+++ supervised_learning/classification/4-neuron.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+"""nueron"""
+import numpy as np
+
+
+class Neuron:
+    """Neuron Class"""
+    def __init__(self, nx):
+        """Cunstructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    def forward_prop(self, X):
+        """Forward Propogation"""
+        self.__A = self.sigmoid(np.matmul(self.__W, X) + self.__b)
+        return self.__A
+
+    def sigmoid(self, X):
+        """Sigmoid"""
+        return 1 / (1 + np.exp(-X))
+
+    def cost(self, Y, A):
+        """model cost"""
+        m = Y.shape[1]
+        cs = (-1 / m) * np.sum(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        """cs = cost"""
+        return cs
+
+    def evaluate(self, X, Y):
+        m = X.shape[1]
+        prediction = np.zeros((1, m))
+
+        A = self.forward_prop(X)
+        prediction[A >= 0.5] = 1
+
+        cs = -np.mean(Y * np.log(A) + (1 - Y) * np.log(1 - A))
+        return prediction, cs
+    @property
+    def W(self):
+        return self.__W
+
+    @property
+    def b(self):
+        return self.__b
+
+    @property
+    def A(self):
+        return self.__A
