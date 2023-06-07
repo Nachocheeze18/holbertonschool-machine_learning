c1157ef235f1c704cd073e08430bebfd484ad7ac
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-07 15:41:58 -0500

    train

---
+++ supervised_learning/classification/22-deep_neural_network.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+"""Neural Network"""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Neural Network Class"""
+    def __init__(self, nx, layers):
+        """Constructor"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list):
+            raise TypeError("layers must be a list of positive integers")
+        if len(layers) < 1 or False in (np.array(layers) > 0):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for i in range(self.__L):
+            if i == 0:
+                j = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.__weights['W' + str(i + 1)] = j
+            else:
+                jjj = np.sqrt(2 / layers[i-1])
+                jj = np.random.randn(layers[i], layers[i-1]) * jjj
+                self.__weights['W' + str(i + 1)] = jj
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    def forward_prop(self, X):
+        """Forward Propogation"""
+        A = X
+        self.__cache['A0'] = X
+
+        for i in range(1, self.__L + 1):
+            W = self.__weights['W' + str(i)]
+            b = self.__weights['b' + str(i)]
+            Z = np.matmul(W, A) + b
+            A = self.sigmoid(Z)
+            self.__cache['A' + str(i)] = A
+
+        return A, self.__cache
+
+    def sigmoid(self, X):
+        """Sigmoid Helper"""
+        return 1 / (1 + np.exp(-X))
+
+    def cost(self, Y, A):
+        """Cost Func"""
+        m = Y.shape[1]
+        j = np.log(1.0000001 - A)
+        return ((-1/m) * np.sum(Y * np.log(A) + (1 - Y) * j))
+
+    def evaluate(self, X, Y):
+        """Evaluate Func"""
+        A, _ = self.forward_prop(X)
+        predictions = np.where(A >= 0.5, 1, 0)
+
+        return predictions, self.cost(Y, A)
+
+    def gradient_descent(self, Y, cache, alpha=0.05):
+        """Gradient Descent"""
+        m = Y.shape[1]
+        L = self.__L
+
+        A = cache["A" + str(L)]
+        dZ = A - Y
+
+        for l in range(L, 0, -1):
+            A_prev = cache["A" + str(l - 1)]
+            W = self.__weights["W" + str(l)]
+            b = self.__weights["b" + str(l)]
+
+            dW = (1 / m) * np.matmul(dZ, A_prev.T)
+            db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+            dA = np.matmul(W.T, dZ)
+
+            self.__weights["W" + str(l)] -= alpha * dW
+            self.__weights["b" + str(l)] -= alpha * db
+
+            if l > 1:
+                dZ = dA * (A_prev * (1 - A_prev))
+
+    def train(self, X, Y, iterations=5000, alpha=0.05):
+        """Trains the neuron"""
+        if not isinstance(iterations, int):
+            raise TypeError("iterations must be an integer")
+        if iterations < 0:
+            raise ValueError("iterations must be a positive integer")
+        if not isinstance(alpha, float):
+            raise TypeError("alpha must be a float")
+        if alpha < 0:
+            raise ValueError("alpha must be positive")
+
+        for _ in range(iterations):
+            self.forward_prop(X)
+            self.gradient_descent(Y, self.__cache, alpha)
+
+        return self.evaluate(X, Y)
+
+    @property
+    def L(self):
+        """layer getter"""
+        return self.__L
+
+    @property
+    def cache(self):
+        '''itermed val getter'''
+        return self.__cache
+
+    @property
+    def weights(self):
+        '''weight getter'''
+        return self.__weights
