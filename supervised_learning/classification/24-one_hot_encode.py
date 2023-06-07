31cefefe5a8b6c68f521924336b2d631b56ac48c
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-07 15:44:04 -0500

    file 24, 25

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""One Hot Encode?"""
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    """Encode Fun"""
+
+    if type(Y) is not np.ndarray:
+        return None
+
+    try:
+        m = Y.shape[0]
+        one_hot_matrix = np.zeros((classes, m))
+
+        for i in range(m):
+            if Y[i] >= 0:
+                one_hot_matrix[Y[i], i] = 1
+
+        return one_hot_matrix
+
+    except Exception:
+        return None
