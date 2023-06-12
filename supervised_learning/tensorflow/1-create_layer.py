5010e0d98bb62a856e18210a92436be3c3d3fa4a
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-12 08:37:28 -0700

    1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -5,15 +5,7 @@ import tensorflow as tf
 
 def create_layer(prev, n, activation, i):
     """layer func"""
-     initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-
-    activation_name = activation.__name__ if activation is not None else ""
-    layer_name = "layer" + "_" + str(i) + activation_name
-
-    layer = tf.layers.dense(inputs=prev,
-                            units=n,
-                            activation=activation,
-                            kernel_initializer=initializer,
-                            name=layer_name)
-
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(prev, n, activation=activation,
+                            kernel_initializer=init)
     return layer
