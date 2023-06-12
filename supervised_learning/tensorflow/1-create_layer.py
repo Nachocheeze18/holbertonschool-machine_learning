cea49ebb552ae3d15bed41b6c504a891fc2c8ba1
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-11 18:03:36 -0700

    1-create_layer.py, accidently used keras

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -7,6 +7,9 @@ import tensorflow as tf
 def create_layer(prev, n, activation):
 """layer func"""
     initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.Dense(units=n, activation=activation, kernel_initializer=initializer, name="layer")
-    output = layer(prev)
+    input_shape = prev.get_shape().as_list()[1:]
+    weights = tf.Variable(initializer([input_shape[-1], n]), name='layer_weights')
+    biases = tf.Variable(tf.zeros([n]), name='layer_biases')
+    layer_output = tf.matmul(prev, weights) + biases
+    output = activation(layer_output)
     return output
