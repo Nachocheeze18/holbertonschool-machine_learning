25fc79aec380ff12978345343c9b8b62d885571e
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-11 20:08:48 -0700

    1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -6,10 +6,12 @@ import tensorflow as tf
 
 
 def create_layer(prev, n, activation):
-"""layer func"""
-    initializer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    """layer func"""
+
+
+    initial = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
     input_shape = prev.get_shape().as_list()[1:]
-    weights = tf.Variable(initializer([input_shape[-1], n]), name='layer_weights')
+    weigh = tf.Variable(initializer([input_shape[-1], n]), name='layer_weights')
     biases = tf.Variable(tf.zeros([n]), name='layer_biases')
     layer_output = tf.matmul(prev, weights) + biases
     output = activation(layer_output)
