db744bda8ee6cebb00e9f29bb782bd9255f9863a
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-12 08:39:16 -0700

    1-create_layer.py

--- supervised_learning/tensorflow/1-create_layer.py
+++ supervised_learning/tensorflow/1-create_layer.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""create layer"""
+"""Creating Forward Prop"""
 import tensorflow as tf
+create_layer = __import__('1-create_layer').create_layer
 
 
-def create_layer(prev, n, activation, i):
-    """layer func"""
-    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layer = tf.layers.dense(prev, n, activation=activation,
-                            kernel_initializer=init)
-    return layer
+def forward_prop(x, layer_sizes=[], activations=[]):
+    """Forward Prop"""
+    for n in range(len(layer_sizes)):
+        x = create_layer(x, layer_sizes[n], activations[n])
+
+    return x
