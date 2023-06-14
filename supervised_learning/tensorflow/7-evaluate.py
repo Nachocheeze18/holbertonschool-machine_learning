5d8729b63072ad744545f3f18c7a399f807c714f
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-14 15:05:29 -0700

    evaluate func

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -4,15 +4,23 @@ import tensorflow as tf
 
 
 def evaluate(X, Y, save_path):
-    """Evaluate Func"""
+"""evaluate func"""
+    saver = tf.train.import_meta_graph(save_path + '.meta')
 
-    sess = tf.Session()
-    saved = tf.train.import_meta_graph(save_path + '.meta')
-    saved.restore(sess, save_path)
+    # Get the tensors from the graph's collection
     graph = tf.get_default_graph()
-    x = graph.get_collection("x")[0]
-    y = graph.get_collection("y")[0]
-    y_pred = graph.get_collection("y_pred")[0]
-    accuracy = graph.get_collection("accuracy")[0]
-    loss = graph.get_collection("loss")[0]
-    return tuple(sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y}))
+    x = graph.get_tensor_by_name('input_placeholder:0')
+    y = graph.get_tensor_by_name('labels_placeholder:0')
+    y_pred = graph.get_tensor_by_name('output_layer/y_pred:0')
+    loss = graph.get_tensor_by_name('loss/loss:0')
+    accuracy = graph.get_tensor_by_name('accuracy/accuracy:0')
+
+    # Start a TensorFlow session
+    with tf.Session() as sess:
+        # Restore the saved model
+        saver.restore(sess, save_path)
+
+        # Evaluate the model on the input data
+        pred, acc, l = sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y})
+
+    return pred, acc, l
