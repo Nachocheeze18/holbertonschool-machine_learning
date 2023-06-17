2e393a41fdad5e81a09a74bf6a702e52c79a58b5
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-17 15:23:00 -0500

    deep neural network

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -2,6 +2,7 @@
 """Neural Network"""
 import numpy as np
 
+
 class DeepNeuralNetwork:
     """deep neural network"""
     def __init__(self, nx, layers):
@@ -10,33 +11,20 @@ class DeepNeuralNetwork:
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
-        if not isinstance(layers, list) or len(layers) == 0:
+        if not isinstance(layers, list):
             raise TypeError("layers must be a list of positive integers")
-        if not all(isinstance(layer, int) and layer > 0 for layer in layers):
+        if len(layers) < 1 or False in (np.array(layers) > 0):
             raise TypeError("layers must be a list of positive integers")
 
-        self.__L = len(layers)
-        self.__cache = {}
-        self.__weights = {}
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
 
-        for l in range(1, self.__L + 1):
-            if l == 1:
+        for i, layer_size in enumerate(layers):
+            if i == 0:
                 input_size = nx
             else:
-                input_size = layers[l-2]
-                
-            layer_size = layers[l-1]
-            self.__weights['W' + str(l)] = np.random.randn(layer_size, input_size) * np.sqrt(2 / input_size)
-            self.__weights['b' + str(l)] = np.zeros((layer_size, 1))
+                input_size = layers[i-1]
 
-    @property
-    def L(self):
-        return self.__L
-
-    @property
-    def cache(self):
-        return self.__cache
-
-    @property
-    def weights(self):
-        return self.__weights
+            self.weights['W' + str(i+1)] = np.random.randn(layer_size, input_size) * np.sqrt(2 / input_size)
+            self.weights['b' + str(i+1)] = np.zeros((layer_size, 1))
