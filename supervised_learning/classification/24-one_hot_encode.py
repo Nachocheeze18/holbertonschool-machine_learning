75e7b32118f570b6f301c777918343e2ae2cda41
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-18 11:21:02 -0500

    one hot encode

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -5,11 +5,19 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """numeric label to matrix"""
-    try:
-        m = len(Y)
-        encoded = np.zeros((classes, m))
-        encoded[Y, np.arange(m)] = 1
-        return encoded
-    except Exception as e:
-        print("Error:", str(e))
+    if type(Y) is not np.ndarray:
         return None
+
+    try:
+        m = Y.shape[0]
+        one_hot_matrix = np.zeros((classes, m))
+
+        for i in range(m):
+            if Y[i] >= 0 and Y[i] < classes:
+                one_hot_matrix[Y[i], i] = 1
+
+        return one_hot_matrix
+
+    except Exception:
+        return None
+
