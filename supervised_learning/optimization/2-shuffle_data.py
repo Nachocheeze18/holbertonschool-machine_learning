f030c3eaa9e2525a4e2ede6228f84000de2d0d32
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-25 13:35:19 -0500

    shufle data

--- supervised_learning/optimization/2-shuffle_data.py
+++ supervised_learning/optimization/2-shuffle_data.py
@@ -7,7 +7,7 @@ def shuffle_data(X, Y):
     """shuffle func"""
     assert X.shape[0] == Y.shape[0]
     m = X.shape[0]
-    perm = np.random.perm(m)
+    perm = np.random.permutation(m)
     _X = X[perm]
     _Y = Y[perm]
     return _X, _Y
