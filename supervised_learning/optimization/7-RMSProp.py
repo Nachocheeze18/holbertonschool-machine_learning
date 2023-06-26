a12584e5a20a8e04d8485bce1b7d3c57eee707b9
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-26 18:28:47 -0500

    rms

---
+++ supervised_learning/optimization/7-RMSProp.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""RMSProp"""
+
+import numpy as np
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """RMS func"""
+    n = beta2 * s + (1 - beta2) * np.power(grad, 2)
+    r = var - alpha * grad / (np.sqrt(new_s) + epsilon)
+    return r, n
