ba21d404fb562d57284accfb2f2b040330ca04a3
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-26 18:36:58 -0500

    rms prop

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     """RMSProp optimization"""
-    op = tf.train.RMSPropOptimizer(learning_rate=alpha, decay=beta2, epsilon=epsilon)
+    op = tf.train.RMSPropOptimizer(learning_rate=alpha,
+     decay=beta2, epsilon=epsilon)
     t = op.minimize(loss)
     return t
