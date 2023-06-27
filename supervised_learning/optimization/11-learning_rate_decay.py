a4831e637a37d90ed898efcdd4497be73de1b440
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-27 12:05:31 -0500

    inverse time decay

--- supervised_learning/optimization/11-learning_rate_decay.py
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -6,5 +6,5 @@ import numpy as np
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     """leaarning rate"""
-    l = alpha / (1 + decay_rate * (global_step // decay_step))
-    return l
+    learn = alpha / (1 + decay_rate * (global_step // decay_step))
+    return learn
