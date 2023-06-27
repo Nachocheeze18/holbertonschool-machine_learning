80073ee5d9cbd9c5a6044016f1b60f663d020716
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-27 12:03:31 -0500

    inverse time decay

---
+++ supervised_learning/optimization/11-learning_rate_decay.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""inverse time decay"""
+
+import numpy as np
+
+
+def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
+    """leaarning rate"""
+    l = alpha / (1 + decay_rate * (global_step // decay_step))
+    return l
