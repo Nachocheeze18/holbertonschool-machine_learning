8bbd9cab025b6963227b45a761ffd270cb3334c9
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-27 11:45:19 -0500

    adam algorithm neural network

---
+++ supervised_learning/optimization/10-Adam.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""train"""
+
+import tensorflow as tf
+
+def create_Adam_op(loss, alpha, beta1, beta2, epsilon):
+    """neural network"""
+    s = tf.Variable(0, trainable=False, name='global_step')
+    op = tf.train.AdamOptimizer(learning_rate=alpha, beta1=beta1, beta2=beta2, epsilon=epsilon)
+    t = op.minimize(loss, global_step=s)
+    return t
