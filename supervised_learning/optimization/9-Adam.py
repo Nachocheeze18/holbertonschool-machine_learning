f9f351cd218863430fc1fbe72fd450af78a274e4
Nachocheeze18 <nachovelez490@gmail.com>
2023-06-26 21:10:11 -0500

    adam op

---
+++ supervised_learning/optimization/9-Adam.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Adam"""
+
+import matplotlib as plt
+import numpy as np
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """adam optimization"""
+    v = beta1 * v + (1 - beta1) * grad
+    s = beta2 * s + (1 - beta2) * np.square(grad)
+    v_corrected = v / (1 - np.power(beta1, t))
+    s_corrected = s / (1 - np.power(beta2, t))
+    var -= alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
+    return var, v, s
