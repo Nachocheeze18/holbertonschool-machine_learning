058f2a26b7e2d6fc119217b13d3a16a91925a181
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-01 17:21:06 -0500

    error analysis

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -13,9 +13,9 @@ def create_confusion_matrix(labels, logits):
 
     confusion_matrix = np.zeros((num_classes, num_classes), dtype=np.int32)
 
-    for sample_idx in range(num_samples):
-        true_label_idx = true_labels[sample_idx]
-        predicted_label_idx = predicted_labels[sample_idx]
-        confusion_matrix[true_label_idx, predicted_label_idx] += 1
+    for i in range(num_samples):
+        true_label_idx = true_labels[i]
+        predicted_label = predicted_labels[i]
+        confusion_matrix[true_label_idx, predicted_label] += 1
 
     return confusion_matrix
