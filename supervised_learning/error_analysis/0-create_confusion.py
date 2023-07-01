8be22e48b099c5abb6ba203ecebee78531954b8f
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-01 17:01:58 -0500

    error analysis

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -6,10 +6,11 @@ import numpy as np
 
 def create_confusion_matrix(labels, logits):
     """frequency and instances"""
-    num_classes = labels.shape[1]
-    matrix = np.zeros((num_classes, num_classes), dtype=int)
-    
-    for a, p in zip(np.argmax(labels, axis=1), np.argmax(logits, axis=1)):
-        matrix[a][p] += 1
-    
-    return matrix
+    num_samples, num_classes = labels.shape
+    true_labels = np.argmax(labels, axis=1)
+    predicted_labels = np.argmax(logits, axis=1)
+    confusion_matrix = np.zeros((num_classes, num_classes), dtype=np.int32)
+    for true_label, predicted_label in zip(true_labels, predicted_labels):
+        confusion_matrix[true_label, predicted_label] += 1
+
+    return confusion_matrix
