6d9732a20575568f81edd6269f3297a1e059849d
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-01 17:58:03 -0500

    specficity

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -8,11 +8,12 @@ def specificity(confusion):
     """calculates the specificity for each class """
     classes = confusion.shape[0]
     specificity_arr = np.zeros(classes)
-    
+
     for i in range(classes):
-        t_negative = np.sum(np.delete(np.delete(confusion, i, axis=0), i, axis=1))
+        t_negative = np.sum(np.delete(np.delete(confusion, i, axis=0),
+                                      i, axis=1))
         f_positive = np.sum(confusion[:, i]) - confusion[i, i]
         specificity = t_negative / (t_negative + f_positive)
         specificity_arr[i] = specificity
-    
+
     return specificity_arr
