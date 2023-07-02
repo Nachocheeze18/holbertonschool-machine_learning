6e522238c926e69840679299f841c962e4e4cc9b
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-02 14:38:56 -0500

    layer that includes L2 regularization

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/env python3
-""""""
+"""Layers"""
 
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """ layer that includes L2 regularization"""
-    initialize = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    regularize = tf.contrib.layers.l2_regularizer(scale=lambtha)
+    """layer that includes L2 regularization"""
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    reg = tf.contrib.layers.l2_regularizer(scale=lambtha)
     layer = tf.layers.Dense(units=n, activation=activation,
-                            kernel_initializer=initialize, kernel_regularizer=regularize)
+                            kernel_initializer=init, kernel_regularizer=reg)
     output = layer(prev)
     return output
