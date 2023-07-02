d1e93c0b3233e50ad8925c372f6464776e8fd8a4
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-02 14:15:51 -0500

    gradient descent with l2 regurlarzation

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -6,8 +6,4 @@ import tensorflow as tf
 
 def l2_reg_cost(cost):
     """l2 regularization in tensorflow"""
-    lambtha = tf.constant(0.01)  # regularization parameter
-    l2_cost = lambtha * tf.reduce_sum
-    ([tf.nn.l2_loss(var) for var in tf.trainable_variables()])
-    total_cost = cost + l2_cost
-    return total_cost
+    return (cost + tf.reduce_sum(tf.losses.get_regularization_losses()))
