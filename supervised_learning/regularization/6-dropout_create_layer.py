0b76a59b479eb951ce79043c90f1ee050b7335bf
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-02 17:28:56 -0500

    dropout create layer

--- supervised_learning/regularization/6-dropout_create_layer.py
+++ supervised_learning/regularization/6-dropout_create_layer.py
@@ -6,9 +6,13 @@ import tensorflow as tf
 
 def dropout_create_layer(prev, n, activation, keep_prob):
     """creates layer using dropout"""
-    w = tf.Variable(tf.random.normal([prev.shape[1], n]))
-    b = tf.Variable(tf.zeros([n]))
-    lin_out = tf.matmul(prev, w) + b
-    act_out = activation(lin_out)
-    dropout_out = tf.nn.dropout(act_out, keep_prob)
-    return dropout_out
+    init = tf.initializers.VarianceScaling(mode="FAN_AVG")
+    reg = tf.keras.regularizers.l2(0.1)
+    
+    dense = tf.keras.layers.Dense(n, activation=activation,
+                                  kernel_initializer=init,
+                                  kernel_regularizer=reg)(prev)
+    
+    dropout = tf.keras.layers.Dropout(1 - keep_prob)(dense)
+    
+    return dropout
