a9bd95020f2d6145acca1e88f2be7799506429b7
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-02 18:25:22 -0500

    early stop

--- supervised_learning/regularization/7-early_stopping.py
+++ supervised_learning/regularization/7-early_stopping.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """stop descent"""
 
+
 def early_stopping(cost, opt_cost, threshold, patience, count):
     """early stopping conditions"""
     count = 0 if (opt_cost - cost) > threshold else count + 1
