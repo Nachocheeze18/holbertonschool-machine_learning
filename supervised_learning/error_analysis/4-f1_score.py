e5b448d8549e0ad85d6d818822916ac7e861e19d
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-01 21:56:23 -0500

    f1 score

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -8,19 +8,7 @@ precision = __import__('2-precision').precision
 
 def f1_score(confusion):
     """calculates the true positives, false positives, and false negatives"""
-    classes = confusion.shape[0]
-    f1_scores = np.zeros(classes)
-
-    for i in range(classes):
-        true_positive = confusion[i, i]
-        false_positive = np.sum(confusion[:, i]) - true_positive
-        false_negative = np.sum(confusion[i, :]) - true_positive
-
-        if true_positive == 0:
-            f1_scores[i] = 0.0
-        else:
-            p = precision(true_positive, false_positive)
-            s = sensitivity(true_positive, false_negative)
-            f1_scores[i] = 2 * (p * s) / (p + s)
-
-    return f1_scores
+    n = precision(confusion) * sensitivity(confusion)
+    d = precision(confusion) + sensitivity(confusion)
+    f1 = 2 * (n / d)
+    return f1
