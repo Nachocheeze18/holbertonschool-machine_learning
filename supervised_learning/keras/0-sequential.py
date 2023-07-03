f39169feb95371addce70fbef62cae5e5f4e2441
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-03 11:18:29 -0500

    keras

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -7,21 +7,13 @@ import tensorflow.keras as K
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
     """Keras library"""
-    model = k.models.Sequential()
-    for i, (units, activation) in enumerate(zip(layers, activations)):
-        if i == 0:
-            model.add(k.layers.Dense(units, activation=activation,
-                                     input_shape=(nx,),
-                                     kernel_regularizer=k.regularizers.l2(lambtha)))
-        else:
-            model.add(k.layers.Dense(units, activation=activation,
-                                     kernel_regularizer=k.regularizers.l2(lambtha)))
-        if i < len(layers) - 1:
-            model.add(k.layers.Dropout(1 - keep_prob))
+    inputs = k.Input(shape=(nx,))
+    x = inputs
+
+    for units, activation in zip(layers, activations):
+        x = k.layers.Dense(units, activation=activation,
+                           kernel_regularizer=k.regularizers.l2(lambtha))(x)
+        x = k.layers.Dropout(1 - keep_prob)(x)
+
+    model = k.Model(inputs=inputs, outputs=x)
     return model
-
-
-if __name__ == '__main__':
-    network = build_model(784, [256, 256, 10], ['tanh', 'tanh', 'softmax'], 0.001, 0.95)
-    network.summary()
-    print(network.losses)
