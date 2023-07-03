d9aa93a4ec377e54eeb85ecaefdaefc04dd6d718
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-03 11:10:27 -0500

    keras

--- supervised_learning/keras/0-sequential.py
+++ supervised_learning/keras/0-sequential.py
@@ -9,14 +9,15 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
     """Keras library"""
     model = k.models.Sequential()
     for i in range(len(layers)):
-        kwargs = {'activation': activations[i]}
         if i == 0:
-            kwargs['input_shape'] = (nx,)
-            kwargs['kernel_regularizer'] = k.regularizers.l2(lambtha)
+            model.add(k.layers.Dense(layers[i], activation=activations[i],
+                                     input_shape=(nx,),
+                                     kernel_regularizer=k.regularizers.l2(
+                                         lambtha)))
         else:
-            kwargs['kernel_regularizer'] = k.regularizers.l2(lambtha)
-        model.add(k.layers.Dense(layers[i], **kwargs))
-        
+            model.add(k.layers.Dense(layers[i], activation=activations[i],
+                                     kernel_regularizer=k.regularizers.l2(
+                                         lambtha)))
         if i < len(layers) - 1:
             model.add(k.layers.Dropout(1 - keep_prob))
     return model
