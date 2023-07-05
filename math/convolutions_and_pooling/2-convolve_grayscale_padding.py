72324ccc8b5cd462b683d710b19746bf7ff01b65
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-05 15:11:38 -0500

    pycodestyl

--- math/convolutions_and_pooling/2-convolve_grayscale_padding.py
+++ math/convolutions_and_pooling/2-convolve_grayscale_padding.py
@@ -6,9 +6,9 @@ import numpy as np
 def convolve_grayscale_same(images, kernel, padding):
     """By using this function, you can perform
     a convolution on grayscale images with a
-    specified kernel and custom padding, 
-    obtaining transformed images that 
-    incorporate the padding to preserve 
+    specified kernel and custom padding,
+    obtaining transformed images that
+    incorporate the padding to preserve
     the image dimensions during convolution.."""
 
     kh = kernel.shape[0]
@@ -21,7 +21,7 @@ def convolve_grayscale_same(images, kernel, padding):
     h = images.shape[1] + (2 * padding[0]) - kh + 1
     w = images.shape[2] + (2 * padding[1]) - kw + 1
 
-    pad = np.pad(images, ((0, 0), (ph , ph),
+    pad = np.pad(images, ((0, 0), (ph, ph),
                           (pw,  pw)), mode='constant')
 
     convolved_images = np.zeros((m, h, w))
