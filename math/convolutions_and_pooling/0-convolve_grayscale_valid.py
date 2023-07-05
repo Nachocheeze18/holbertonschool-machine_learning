bd13e3a1b7d64cfc86cd5de10c4fe0a8e2ce7670
Nachocheeze18 <5581@holbertonstudents.com>
2023-07-04 21:40:28 -0500

    valiod convolution on grayscale images

--- math/convolutions_and_pooling/0-convolve_grayscale_valid.py
+++ math/convolutions_and_pooling/0-convolve_grayscale_valid.py
@@ -5,24 +5,18 @@ import numpy as np
 
 def convolve_grayscale_valid(images, kernel):
     """function that performs a valid convolution on grayscale images"""
-    m, h, w = images.shape
-    kh, kw = kernel.shape
-    convolved_images = np.zeros((m, h - kh + 1, w - kw + 1))
-    
-    for i in range(m):
-        for j in range(h - kh + 1):
-            for k in range(w - kw + 1):
-                convolved_images[i, j, k] = np.sum(images[i, j:j+kh, k:k+kw] * kernel)
-    
-    return convolved_images
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
 
-np.random.seed(0)
-m = np.random.randint(1000, 2000)
-h, w = np.random.randint(100, 200, 2).tolist()
-fh, fw = np.random.randint(3, 10, 2).tolist()
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
 
-images = np.random.randint(0, 256, (m, h, w))
-kernel = np.random.randint(0, 10, (fh, fw))
-conv_ims = convolve_grayscale_valid(images, kernel)
-print(conv_ims)
-print(conv_ims.shape)
+    image = np.zeros((m, h - kh + 1, w - kw + 1))
+
+    for j in range(h - kh + 1):
+        for k in range(w - kw + 1):
+            i = np.multiply(images[:, j:j + kh, k:k + kw], kernel)
+            image[:, j, k] = np.sum(i, axis = (1, 2))
+
+    return image
