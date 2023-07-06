469ff68754351f41d721ec34282af8e1aeaddd2e
Nachocheeze18 <5581@holbertonstudents.com>
2023-07-05 20:06:26 -0500

    one_hot

--- supervised_learning/keras/3-one_hot.py
+++ supervised_learning/keras/3-one_hot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""imports"""
+"""one_hot"""
 
-Import tensorflow.keras as K
+import tensorflow.keras as K
 
 
 def one_hot(labels, cls=None):
@@ -10,8 +10,5 @@ def one_hot(labels, cls=None):
     each row represents a label, and the last dimension
     corresponds to the number of classes, with a value of
     1 indicating the presence of that class and 0 otherwise."""
-    if cls is None:
-        max_label = max(labels)
-        cls = max_label + 1
-    one_hot_matrix = K.utils.to_categorical(labels, num_classes=cls)
-    return one_hot_matrix
+    one_hot = K.utils.to_categorical(labels, cls)
+    return one_hot
