f199b83f0fe556f7592af2b63ec93e69a6bfde79
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-11 14:58:53 -0500

    pool forward prop

---
+++ supervised_learning/cnn/2-conv_backward.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""back prop"""
+
+import numpy as np
+
+
+def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
+    """ performs back propagation over a convolutional
+    layer of a neural network"""
+    m = dZ.shape[0]
+    h_new = dZ.shape[1]
+    w_new = dZ.shape[2]
+    c_new = dZ.shape[3]
+
+    m = A_prev.shape[0]
+    h_prev = A_prev.shape[1]
+    w_prev = A_prev.shape[2]
+    c_prev = A_prev.shape[3]
+
+    kh = W.shape[0]
+    kw = W.shape[1]
+    c_prev = W.shape[2]
+    c_new = W.shape[3]
+
+    sh = stride[0]
+    sw = stride[1]
+
+    if padding == "same":
+        ph = ((h_prev - 1) * sh + kh - h_prev) // 2
+        pw = ((w_prev - 1) * sw + kw - w_prev) // 2
+    elif padding == "valid":
+        ph = 0
+        pw = 0
+
+    A_prev_pad = np.pad(
+        A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)), mode='constant')
+    dA_prev_pad = np.zeros_like(A_prev_pad)
+    dW = np.zeros_like(W)
+    db = np.zeros_like(b)
+
+    for i in range(m):
+        for h in range(h_new):
+            for w in range(w_new):
+                for c in range(c_new):
+                    h_start = h * sh
+                    h_end = h_start + kh
+                    w_start = w * sw
+                    w_end = w_start + kw
+
+                    A_slice = A_prev_pad[i, h_start:h_end, w_start:w_end, :]
+                    dZ_value = dZ[i, h, w, c]
+
+                    dA_prev_pad[i, h_start:h_end,
+                                w_start:w_end,
+                                :] += W[:, :, :,
+                                        c] * dZ_value[np.newaxis,
+                                                      np.newaxis]
+                    dW[:, :, :, c] += A_slice * dZ_value
+                    db[:, :, :, c] += dZ_value
+
+    if padding == "same":
+        dA_prev = dA_prev_pad[:, ph:-ph, pw:-pw, :]
+    elif padding == "valid":
+        dA_prev = dA_prev_pad
+
+    return dA_prev, dW, db
