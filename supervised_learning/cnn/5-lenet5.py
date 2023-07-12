a56c9d8ff858d6ced3694aa6382c4cfda41d0203
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-12 15:51:18 -0500

    task 5

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -7,19 +7,18 @@ import tensorflow.keras as K
 def lenet5(X):
     """LeNet-5 model with the specified architecture,
     compiled with the Adam optimizer and accuracy metrics."""
-    init = K.initializers.he_normal()
 
     c1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5),
                          padding='same',
                          activation='relu',
-                         kernel_initializer=init)(X)
+                         kernel_initializer='he_normal')(X)
 
     p1 = K.layers.MaxPooling2D(pool_size=(2, 2),
                                strides=(2, 2))(c1)
 
     c2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5),
                          padding='valid', activation='relu',
-                         kernel_initializer=init)(p1)
+                         kernel_initializer='he_normal')(p1)
 
     p2 = K.layers.MaxPooling2D(pool_size=(2, 2),
                                strides=(2, 2))(c2)
@@ -27,14 +26,14 @@ def lenet5(X):
     flat = K.layers.Flatten()(p2)
 
     fc1 = K.layers.Dense(units=120, activation='relu',
-                         kernel_initializer=init)(flat)
+                         kernel_initializer='he_normal')(flat)
 
     fc2 = K.layers.Dense(units=84, activation='relu',
-                         kernel_initializer=init)(fc1)
+                         kernel_initializer='he_normal')(fc1)
 
     out = K.layers.Dense(units=10,
                          activation='softmax',
-                         kernel_initializer=init)(fc2)
+                         kernel_initializer='he_normal')(fc2)
 
     model = K.models.Model(X, out)
 
