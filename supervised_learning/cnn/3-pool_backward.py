9005a91e534c6cfecb1edf4aacc015690f2d90f6
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-12 14:48:17 -0500

    task 3

--- supervised_learning/cnn/3-pool_backward.py
+++ supervised_learning/cnn/3-pool_backward.py
@@ -3,12 +3,24 @@
 
 import numpy as np
 
+
 def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
     """performs back propagation over a pooling layer of a neural network"""
-    m, h_new, w_new, c_new = dA.shape
-    m, h_prev, w_prev, c = A_prev.shape
-    kh, kw = kernel_shape
-    sh, sw = stride
+    m = dA.shape[0]
+    h_new = dA.shape[1]
+    w_new = dA.shape[2]
+    c_new = dA.shape[3]
+
+    m = A_prev.shape[0]
+    h_prev = A_prev.shape[1]
+    w_prev = A_prev.shape[2]
+    c = A_prev.shape[3]
+
+    kh = kernel_shape[0]
+    kw = kernel_shape[1]
+
+    sh = stride[0]
+    sw = stride[1]
 
     dA_prev = np.zeros_like(A_prev)
 
@@ -22,10 +34,15 @@ def pool_backward(dA, A_prev, kernel_shape, stride=(1, 1), mode='max'):
                     h_end = h_start + kw
 
                     if mode == 'max':
-                        mask = (A_prev[img, v_start:v_end, h_start:h_end, c] == np.max(A_prev[img, v_start:v_end, h_start:h_end, c]))
-                        dA_prev[img, v_start:v_end, h_start:h_end, c] += mask * dA[img, h, w, c]
+                        mask = (A_prev[img, v_start:v_end,
+                                       h_start:h_end, c] == np.max(A_prev[img,
+                                                                          v_start:v_end, h_start:h_end, c]))
+                        dA_prev[img, v_start:v_end,
+                                h_start:h_end, c] += mask * dA[img,
+                                                               h, w, c]
                     elif mode == 'avg':
                         avg = dA[img, h, w, c] / (kh * kw)
-                        dA_prev[img, v_start:v_end, h_start:h_end, c] += np.ones((kh, kw)) * avg
+                        dA_prev[img, v_start:v_end, h_start:h_end,
+                                c] += np.ones((kh, kw)) * avg
 
     return dA_prev
