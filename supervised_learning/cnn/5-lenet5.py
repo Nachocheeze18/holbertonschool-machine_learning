7afac861c631e073af26a024d536fa356de66539
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-12 15:33:29 -0500

    task 5

--- supervised_learning/cnn/5-lenet5.py
+++ supervised_learning/cnn/5-lenet5.py
@@ -1,25 +1,26 @@
 #!/usr/bin/env python3
-""""""
+"""Lenet5"""
 
 import tensorflow.keras as K
 
 
 def lenet5(X):
-    """"""
+    """LeNet-5 model with the specified architecture,
+    compiled with the Adam optimizer and accuracy metrics."""
     c1 = K.layers.Conv2D(filters=6, kernel_size=(5, 5),
-                            padding='same',
-                            activation='relu',
-                            kernel_initializer='he_normal')(X)
+                         padding='same',
+                         activation='relu',
+                         kernel_initializer='he_normal')(X)
 
     p1 = K.layers.MaxPooling2D(pool_size=(2, 2),
                                   strides=(2, 2))(c1)
 
     c2 = K.layers.Conv2D(filters=16, kernel_size=(5, 5),
-                            padding='valid', activation='relu',
-                            kernel_initializer='he_normal')(p1)
+                         padding='valid', activation='relu',
+                         kernel_initializer='he_normal')(p1)
 
     p2 = K.layers.MaxPooling2D(pool_size=(2, 2),
-                                  strides=(2, 2))(c2)
+                               strides=(2, 2))(c2)
 
     flat = K.layers.Flatten()(p2)
 
@@ -30,10 +31,12 @@ def lenet5(X):
                          kernel_initializer='he_normal')(fc1)
 
     out = K.layers.Dense(units=10,
-                            activation='softmax')(fc2)
+                         activation='softmax')(fc2)
 
     model = K.Model(inputs=X, outputs=out)
 
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-    
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+
     return model
