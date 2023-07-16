1e234433b29bb3c0ffc27a8b3d0ce1f5857207ce
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-16 14:15:07 -0500

    inception

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """inception block"""
-import tensorflow as tf
+import tensorflow.keras as K
 
 
 def inception_block(A_prev, filters):
@@ -9,22 +9,20 @@ def inception_block(A_prev, filters):
     inception block"""
     F1, F3R, F3, F5R, F5, FPP = filters
 
-    # 1x1 convolution branch
-    c1 = tf.keras.layers.Conv2D(filters=F1, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
+    c1 = K.layers.Conv2D(filters=F1, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
 
-    # 1x1 convolution followed by 3x3 convolution branch
-    c3_reduce = tf.keras.layers.Conv2D(filters=F3R, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
-    c3 = tf.keras.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same', activation='relu')(c3_reduce)
+    c3_reduce = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
 
-    # 1x1 convolution followed by 5x5 convolution branch
-    c5_reduce = tf.keras.layers.Conv2D(filters=F5R, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
-    c5 = tf.keras.layers.Conv2D(filters=F5, kernel_size=(5, 5), padding='same', activation='relu')(c5_reduce)
+    c3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3), padding='same', activation='relu')(c3_reduce)
 
-    # Max pooling followed by 1x1 convolution branch
-    maxpool = tf.keras.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding='same')(A_prev)
-    maxpool_proj = tf.keras.layers.Conv2D(filters=FPP, kernel_size=(1, 1), padding='same', activation='relu')(maxpool)
+    c5_reduce = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1), padding='same', activation='relu')(A_prev)
 
-    # Concatenate the outputs of all branches
-    inception_output = tf.keras.layers.concatenate([c1, c3, c5, maxpool_proj], axis=3)
+    c5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5), padding='same', activation='relu')(c5_reduce)
 
-    return inception_output
+    maxpool = K.layers.MaxPooling2D(pool_size=(3, 3), strides=(1, 1), padding='same')(A_prev)
+
+    maxpool_proj = K.layers.Conv2D(filters=FPP, kernel_size=(1, 1), padding='same', activation='relu')(maxpool)
+
+    incept_out = K.layers.concatenate([c1, c3, c5, maxpool_proj], axis=3)
+
+    return incept_out
