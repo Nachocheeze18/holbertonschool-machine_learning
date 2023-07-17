fc49b7daa15fda534a92845d0138d4dc844a4c32
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-17 09:48:40 -0500

    inception network

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -92,7 +92,6 @@ def inception_network():
     softmax = K.layers.Dense(units=1000,
                              activation='softmax'
                              )(dropout)
-    model.summary()
 
     model = K.Model(inputs=inputs, outputs=softmax)
 
