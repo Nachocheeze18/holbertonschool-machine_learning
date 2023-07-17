b06d52f3352b74dc47b9ecf69962c572aa545600
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-17 09:58:28 -0500

    inception network

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -45,42 +45,33 @@ def inception_network():
                                    padding='same'
                                    )(x)
 
-    x = inception_block(x,
-                                [64, 96, 128, 16, 32, 32])
+    x = inception_block(x, [64, 96, 128, 16, 32, 32])
 
-    x = inception_block(x,
-                                [128, 128, 192, 32, 96, 64])
+    x = inception_block(x, [128, 128, 192, 32, 96, 64])
 
     x = K.layers.MaxPooling2D(pool_size=(3, 3),
                                      strides=2,
                                      padding='same'
                                      )(x)
 
-    x = inception_block(x,
-                                [192, 96, 208, 16, 48, 64])
+    x = inception_block(x, [192, 96, 208, 16, 48, 64])
 
-    x = inception_block(x,
-                                [160, 112, 224, 24, 64, 64])
+    x = inception_block(x, [160, 112, 224, 24, 64, 64])
 
-    x = inception_block(x,
-                                [128, 128, 256, 24, 64, 64])
+    x = inception_block(x, [128, 128, 256, 24, 64, 64])
 
-    x = inception_block(x,
-                                [112, 144, 288, 32, 64, 64])
+    x = inception_block(x, [112, 144, 288, 32, 64, 64])
 
-    x = inception_block(x,
-                                [256, 160, 320, 32, 128, 128])
+    x = inception_block(x, [256, 160, 320, 32, 128, 128])
 
     x = K.layers.MaxPooling2D(pool_size=(3, 3),
                                      strides=2,
                                      padding='same'
                                      )(x)
 
-    x = inception_block(x,
-                                [256, 160, 320, 32, 128, 128])
+    x = inception_block(x, [256, 160, 320, 32, 128, 128])
 
-    x = inception_block(x,
-                                [384, 192, 384, 48, 128, 128])
+    x = inception_block(x, [384, 192, 384, 48, 128, 128])
 
     x = K.layers.AveragePooling2D(pool_size=(7, 7),
                                          strides=1,
