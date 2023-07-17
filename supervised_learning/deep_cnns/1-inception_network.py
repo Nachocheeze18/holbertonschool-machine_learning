17546cc4afc71b55e328277f758e7e85a567ed54
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-17 10:13:14 -0500

    inception network

--- supervised_learning/deep_cnns/1-inception_network.py
+++ supervised_learning/deep_cnns/1-inception_network.py
@@ -13,34 +13,69 @@ def inception_network():
     with its various convolutional, pooling, and inception
     block layers, culminating in a classification output layer.
     """
-    input = K.Input(shape=(224, 224, 3))
+    inputs = K.Input(shape=(224, 224, 3))
 
-    x = K.layers.Conv2D(64, (7, 7), strides=(
-        2, 2), padding='same', activation='relu')(input)
-    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
-    x = K.layers.Conv2D(64, (1, 1), padding='same', activation='relu')(x)
-    x = K.layers.Conv2D(192, (3, 3), padding='same', activation='relu')(x)
-    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
+    x = K.layers.Conv2D(filters=64,
+                         kernel_size=(7, 7),
+                         strides=2,
+                         padding='same',
+                         activation='relu'
+                         )(inputs)
 
-    # Inception blocks
-    x = inception_block(x, (64, 96, 128, 16, 32, 32))
-    x = inception_block(x, (128, 128, 192, 32, 96, 64))
-    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
-    x = inception_block(x, (192, 96, 208, 16, 48, 64))
-    x = inception_block(x, (160, 112, 224, 24, 64, 64))
-    x = inception_block(x, (128, 128, 256, 24, 64, 64))
-    x = inception_block(x, (112, 144, 288, 32, 64, 64))
-    x = inception_block(x, (256, 160, 320, 32, 128, 128))
-    x = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(x)
-    x = inception_block(x, (256, 160, 320, 32, 128, 128))
-    x = inception_block(x, (384, 192, 384, 48, 128, 128))
+    x = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                 strides=2,
+                                 padding='same'
+                                 )(x)
 
-    # Final layers
-    x = K.layers.GlobalAveragePooling2D()(x)
+    x = K.layers.Conv2D(filters=64,
+                         kernel_size=(1, 1),
+                         padding='valid',
+                         activation='relu'
+                         )(x)
+
+    x = K.layers.Conv2D(filters=192,
+                         kernel_size=(3, 3),
+                         strides=1,
+                         padding='same',
+                         activation='relu'
+                         )(x)
+
+    x = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                   strides=2,
+                                   padding='same'
+                                   )(x)
+
+    x = inception_block(x, [64, 96, 128, 16, 32, 32])
+    x = inception_block(x, [128, 128, 192, 32, 96, 64])
+
+    x = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                     strides=2,
+                                     padding='same'
+                                     )(x)
+
+    x = inception_block(x, [192, 96, 208, 16, 48, 64])
+    x = inception_block(x, [160, 112, 224, 24, 64, 64])
+    x = inception_block(x, [128, 128, 256, 24, 64, 64])
+    x = inception_block(x, [112, 144, 288, 32, 64, 64])
+    x = inception_block(x, [256, 160, 320, 32, 128, 128])
+
+    x = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                     strides=2,
+                                     padding='same'
+                                     )(x)
+
+    x = inception_block(x, [256, 160, 320, 32, 128, 128])
+    x = inception_block(x, [384, 192, 384, 48, 128, 128])
+
+    x = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                         strides=1,
+                                         padding='valid'
+                                         )(x)
+
     x = K.layers.Dropout(0.4)(x)
-    x = K.layers.Dense(1000, activation='softmax')(x)
 
-    # Create the Keras model
-    model = K.Model(inputs=input, outputs=x)
+    x = K.layers.Dense(units=1000, activation='softmax')(x)
+
+    model = K.Model(inputs=inputs, outputs=x)
 
     return model
