9ff3c2ed158ac635b23a6968c02437bdc01848cb
Nachocheeze18 <nachovelez490@gmail.com>
2023-07-16 23:00:15 -0500

    resnet50

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -14,53 +14,52 @@ def resnet50():
     and projection) as described in the "Deep Residual Learning for Image
     Recognition" paper. The function returns the constructed ResNet-50
     model using Keras."""
-    input = (224, 224, 3)
+    inputs = K.Input(shape=(224, 224, 3))
+    init = K.initializers.he_normal()
 
-    input_layer = K.Input(shape=input)
+    c1 = K.layers.Conv2D(filters=64,
+                            kernel_size=(7, 7),
+                            strides=2,
+                            padding='same',
+                            kernel_initializer=init
+                            )(inputs)
 
-    x = K.layers.Conv2D(filters=64,
-                        kernel_size=(7, 7),
-                        strides=2,
-                        padding='same',
-                        kernel_initializer='he_normal'
-                        )(input_layer)
+    b_1 = K.layers.BatchNormalization(axis=3)(c1)
 
-    x = K.layers.BatchNormalization(axis=3)(x)
+    R_1 = K.layers.ReLU()(b_1)
 
-    x= K.layers.ReLU()(x)
+    Max1 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                     strides=2,
+                                     padding='same'
+                                     )(R_1)
 
-    x = K.layers.MaxPooling2D(pool_size=(3, 3),
-                              strides=2,
-                              padding='same'
-                              )(x)
+    c2_a = projection_block(Max1, [64, 64, 256], s=1)
+    c2_b = identity_block(c2_a, [64, 64, 256])
+    c2_c = identity_block(c2_b, [64, 64, 256])
 
-    x = projection_block(x, [64, 64, 256], s=1)
-    x = identity_block(x, [64, 64, 256])
-    x = identity_block(x, [64, 64, 256])
+    c3_a = projection_block(c2_c, [128, 128, 512])
+    c3_b = identity_block(c3_a, [128, 128, 512])
+    c3_c = identity_block(c3_b, [128, 128, 512])
+    c3_d = identity_block(c3_c, [128, 128, 512])
 
-    x = projection_block(x, [128, 128, 512])
-    x = identity_block(x, [128, 128, 512])
-    x = identity_block(x, [128, 128, 512])
-    x = identity_block(x, [128, 128, 512])
+    c4_a = projection_block(c3_d, [256, 256, 1024])
+    c4_b = identity_block(c4_a, [256, 256, 1024])
+    c4_c = identity_block(c4_b, [256, 256, 1024])
+    c4_d = identity_block(c4_c, [256, 256, 1024])
+    c4_e = identity_block(c4_d, [256, 256, 1024])
+    c4_f = identity_block(c4_e, [256, 256, 1024])
 
-    x = projection_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
+    c5_a = projection_block(c4_f, [512, 512, 2048])
+    c5_b = identity_block(c5_a, [512, 512, 2048])
+    c5_c = identity_block(c5_b, [512, 512, 2048])
 
-    x = projection_block(x, [512, 512, 2048])
-    x = identity_block(x, [512, 512, 2048])
-    x = identity_block(x, [512, 512, 2048])
+    AvgPool = K.layers.AveragePooling2D(pool_size=(7, 7),
+                                        strides=1,
+                                        padding='valid'
+                                        )(c5_c)
 
-    x = K.layers.AveragePooling2D(pool_size=(7, 7),
-                                  strides=1,
-                                  padding='valid'
-                                  )(x)
+    softmax = K.layers.Dense(units=1000,
+                             activation='softmax'
+                             )(AvgPool)
 
-    x = K.layers.Dense(units=1000,
-                       activation='softmax'
-                       )(x)
-
-    return K.Model(inputs=input, outputs=x)
+    return K.Model(inputs=inputs, outputs=softmax)
