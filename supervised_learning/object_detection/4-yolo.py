afc497216d10d6c86779fe1ee17cad1b9faa4db9
Nachocheeze18 <nachovelez490@gmail.com>
2023-08-22 18:19:58 -0500

    load images

---
+++ supervised_learning/object_detection/4-yolo.py
@@ -0,0 +1,168 @@
+#!/usr/bin/env python3
+"""imports"""
+import numpy as np
+import os
+import cv2
+import tensorflow.keras as K
+from tensorflow.keras.models import load_model
+
+
+class Yolo:
+    """yolo class"""
+    def __init__(self, model_path, classes_path,
+                 class_threshold, nms_threshold, anchors):
+        """It is designed to load a pre-trained YOLO model,
+        preprocess input images, run object detection on the
+        images, and post-process the model's outputs to obtain
+        the detected objects and their associated information."""
+        self.model = self._load_yolo_model(model_path)
+        self.class_names = self._load_classes(classes_path)
+        self.class_t = class_threshold
+        self.nms_t = nms_threshold
+        self.anchors = anchors
+
+    def _load_yolo_model(self, model_path):
+        """Loads the YOLO model from a file"""
+        return load_model(model_path)
+
+    def _load_classes(self, classes_path):
+        """Loads the classes from a file"""
+        with open(classes_path, 'r') as f:
+            class_names = f.readlines()
+        class_names = [c.strip() for c in class_names]
+        return class_names
+
+    def process_outputs(self, outputs, image_size):
+        """Process Darknet outputs"""
+        boxes = []
+        box_confidences = []
+        box_class_probs = []
+
+        for i in range(len(outputs)):
+            boxes.append(outputs[i][..., :4])
+            box_confidences.append(1 / (1 + np.exp(-outputs[i][..., 4:5])))
+            box_class_probs.append(1 / (1 + np.exp(-outputs[i][..., 5:])))
+
+        image_height, image_width = image_size
+
+        for i in range(len(boxes)):
+            grid_width = outputs[i].shape[1]
+            grid_height = outputs[i].shape[0]
+            anchor_boxes = outputs[i].shape[2]
+            for j in range(grid_height):
+                for k in range(grid_width):
+                    for l in range(anchor_boxes):
+                        tx, ty, tw, th = boxes[i][j, k, l]
+                        pw, ph = self.anchors[i][l]
+                        bx = (1 / (1 + np.exp(-tx))) + k
+                        by = (1 / (1 + np.exp(-ty))) + j
+                        bw = pw * np.exp(tw)
+                        bh = ph * np.exp(th)
+                        bx /= grid_width
+                        by /= grid_height
+                        bw /= self.model.input.shape[1].value
+                        bh /= self.model.input.shape[2].value
+                        x1 = (bx - (bw / 2)) * image_width
+                        y1 = (by - (bh / 2)) * image_height
+                        x2 = (bx + (bw / 2)) * image_width
+                        y2 = (by + (bh / 2)) * image_height
+                        boxes[i][j, k, l] = [x1, y1, x2, y2]
+
+        return boxes, box_confidences, box_class_probs
+
+    def filter_boxes(self, boxes, box_confidences, box_class_probs):
+        """ YOLO model outputs by discarding low-confidence detections,
+        then applies non-maximum suppression to retain only the most
+        confident and non-overlapping bounding boxes, returning the
+        filtered bounding boxes, their corresponding class indices,
+        and confidence scores."""
+        filtered_boxes = None
+        box_classes_list = []
+        box_scores_list = []
+        for i in range(len(boxes)):
+            new_box_score = box_confidences[i] * box_class_probs[i]
+            new_box_class = np.argmax(new_box_score, axis=-1)
+            new_box_score = np.max(new_box_score, axis=-1)
+
+            box_classes_list.append(new_box_class.reshape(-1))
+            box_scores_list.append(new_box_score.reshape(-1))
+
+        box_scores_all = np.concatenate(box_scores_list)
+        box_classes_all = np.concatenate(box_classes_list)
+        box_mask = box_scores_all >= self.class_t
+
+        filtered_boxes = np.concatenate(
+            [box.reshape(-1, 4) for box in boxes], axis=0)
+        filtered_boxes = filtered_boxes[box_mask]
+
+        box_classes = box_classes_all[box_mask]
+        box_scores = box_scores_all[box_mask]
+
+        return filtered_boxes, box_classes, box_scores
+
+    def calculate_iou(self, box1, box2):
+        """calculate the Intersection over Union (IoU)
+        between two bounding boxes, which measures the
+        extent of overlap between the two boxes in relation
+        to their total area, returning a value indicating
+        the degree of overlap."""
+        x1_overlap = max(box1[0], box2[0])
+        y1_overlap = max(box1[1], box2[1])
+        x2_overlap = min(box1[2], box2[2])
+        y2_overlap = min(box1[3], box2[3])
+
+        if x2_overlap > x1_overlap and y2_overlap > y1_overlap:
+            intersection_area = (x2_overlap - x1_overlap) * (y2_overlap
+                                                             - y1_overlap)
+            box1_area = (box1[2] - box1[0]) * (box1[3] - box1[1])
+            box2_area = (box2[2] - box2[0]) * (box2[3] - box2[1])
+
+            union_area = box1_area + box2_area - intersection_area
+            iou = intersection_area / union_area
+            return iou
+        else:
+            return 0.0
+
+    def non_max_suppression(self, filtered_boxes, box_classes, box_scores):
+        """Applies non-maximum suppression to the filtered boxes."""
+        unique_classes = np.unique(box_classes)
+        box_predictions = []
+        predicted_box_classes = []
+        predicted_box_scores = []
+
+        for cls in unique_classes:
+            cls_indices = np.where(box_classes == cls)[0]
+            cls_boxes = filtered_boxes[cls_indices]
+            cls_box_scores = box_scores[cls_indices]
+
+            while len(cls_boxes) > 0:
+                max_score_idx = np.argmax(cls_box_scores)
+                box_predictions.append(cls_boxes[max_score_idx])
+                predicted_box_classes.append(cls)
+                predicted_box_scores.append(cls_box_scores[max_score_idx])
+
+                iou_scores = [self.calculate_iou(cls_boxes[max_score_idx], box)
+                              for box in cls_boxes]
+                to_remove = np.where(np.array(iou_scores) > self.nms_t)[0]
+                cls_boxes = np.delete(cls_boxes, to_remove, axis=0)
+                cls_box_scores = np.delete(cls_box_scores, to_remove)
+
+        return (np.array(box_predictions),
+                np.array(predicted_box_classes),
+                np.array(predicted_box_scores))
+
+    @staticmethod
+    def load_images(folder_path):
+        """Load images from the specified folder path"""
+        images = []
+        image_paths = []
+
+        for filename in os.listdir(folder_path):
+            if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+                image_path = os.path.join(folder_path, filename)
+                image = cv2.imread(image_path)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert BGR to RGB
+                images.append(image)
+                image_paths.append(image_path)
+
+        return images, image_paths
