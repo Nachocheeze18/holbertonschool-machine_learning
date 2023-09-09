08b14cec08d86a12e61e4efa5420ff80fb43c8ec
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-09 11:32:46 -0500

    determinant

--- math/advanced_linear_algebra/0-determinant.py
+++ math/advanced_linear_algebra/0-determinant.py
@@ -6,30 +6,30 @@ def determinant(matrix):
     """
     Calculate the determinant of a square matrix.
     """
-    if not isinstance(matrix, list):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
-
-    if len(matrix) != len(matrix[0]):
-        raise ValueError("matrix must be a square matrix")
-
-    if len(matrix) == 0:
+    
+    num_rows = len(matrix)
+    for row in matrix:
+        if len(row) != num_rows:
+            raise ValueError("matrix must be a square matrix")
+    
+    if num_rows == 0:
         return 1
-
-    if len(matrix) == 1:
+    
+    if num_rows == 1:
         return matrix[0][0]
-
-    if len(matrix) == 2:
+    
+    if num_rows == 2:
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
-
-    result = 0
-    i = 0
-    while i < len(matrix):
-        sub = [[matrix[j][k] for k in range(len(matrix[j])) if k != i] for j in range(1, len(matrix))]
-        cofactor = matrix[0][i] * determinant(sub)
-        if i % 2 == 0:
-            result += cofactor
+    
+    det = 0
+    for col in range(num_rows):
+        submatrix = [row[:col] + row[col + 1:] for row in matrix[1:]]
+        cofactor = matrix[0][col] * determinant(submatrix)
+        if col % 2 == 0:
+            det += cofactor
         else:
-            result -= cofactor
-        i += 1
-
-    return result
+            det -= cofactor
+    
+    return det
