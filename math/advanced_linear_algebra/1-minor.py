fa8ac3d6bb97a469bc8414e9500f36ee3d160bd1
Nachocheeze18 <5581@holbertonstudents.com>
2023-09-11 16:49:02 -0500

    minor matrix

---
+++ math/advanced_linear_algebra/1-minor.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+"""Minor Matrix"""
+
+
+def minor(matrix):
+    """calculates the minor marix of a matrix"""
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    num_rows = len(matrix)
+    if num_rows == 0 or any(len(row) != num_rows for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    minor_matrix = [[0 for _ in range(num_rows)] for _ in range(num_rows)]
+
+    for i in range(num_rows):
+        for j in range(num_rows):
+            submatrix = [row[:j] + row[j+1:] for row in (matrix[:i] + matrix[i+1:])]
+            det = determinant(submatrix)
+            minor_matrix[i][j] = det
+
+    return minor_matrix
+
+def determinant(matrix):
+    """
+    Calculate the determinant of a square matrix.
+    """
+    if type(matrix) is not list or len(matrix) == 0:
+        raise TypeError("matrix must be a list of lists")
+    if len(matrix) == 1 and len(matrix[0]) == 0:
+        return 1
+    for i in range(len(matrix)):
+        if type(matrix[i]) is not list:
+            raise TypeError("matrix must be a list of lists")
+        if len(matrix) != len(matrix[i]):
+            raise ValueError("matrix must be a square matrix")
+
+    num_rows = len(matrix)
+
+    if num_rows == 0:
+        return 1
+
+    if num_rows == 1:
+        return matrix[0][0]
+
+    if num_rows == 2:
+        return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+
+    det = 0
+    for col in range(num_rows):
+        submatrix = [row[:col] + row[col + 1:] for row in matrix[1:]]
+        cofactor = matrix[0][col] * determinant(submatrix)
+        if col % 2 == 0:
+            det += cofactor
+        else:
+            det -= cofactor
+
+    return det
+
