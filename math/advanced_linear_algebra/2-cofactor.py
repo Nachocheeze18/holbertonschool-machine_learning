a0b6428192267c88995003f98d211a0da851b6ed
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 12:23:10 -0500

    COFACTOR MATRIX

--- math/advanced_linear_algebra/2-cofactor.py
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -4,8 +4,7 @@
 
 def cofactor(matrix):
     """calculates the cofactor matrix of a given square matrix."""
-    if not isinstance(matrix, list) or not all(isinstance(row, list)
-                                               for row in matrix):
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
     if not matrix or not matrix[0]:
@@ -15,19 +14,18 @@ def cofactor(matrix):
     if not all(len(row) == num_rows for row in matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
-    def determinant(smatrix):
-        if len(smatrix) == 1:
-            return smatrix[0][0]
-        elif len(smatrix) == 2:
-            return smatrix[0][0] * smatrix[1][1] - smatrix[0][1] * matrix[1][0]
+    def determinant(submatrix):
+        if len(submatrix) == 1:
+            return submatrix[0][0]
+        elif len(submatrix) == 2:
+            return submatrix[0][0] * submatrix[1][1] - submatrix[0][1] * submatrix[1][0]
 
         det = 0
-        for j in range(len(smatrix)):
+        for j in range(len(submatrix)):
             cofactor_sign = (-1) ** j
-            cofactor_value = smatrix[0][j]
-            submatrix_minor = [row[:j] + row[j + 1:] for row in smatrix[1:]]
-            det += cofactor_sign * cofactor_value * determinant
-            (submatrix_minor)
+            cofactor_value = submatrix[0][j]
+            submatrix_minor = [row[:j] + row[j + 1:] for row in submatrix[1:]]
+            det += cofactor_sign * cofactor_value * determinant(submatrix_minor)
         return det
 
     if num_rows == 1:
@@ -37,8 +35,7 @@ def cofactor(matrix):
     for i in range(num_rows):
         cofactor_row = []
         for j in range(num_rows):
-            submatrix = [row[:j] + row[j + 1:] for row in
-                         (matrix[:i] + matrix[i + 1:])]
+            submatrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
             cofactor_sign = (-1) ** (i + j)
             cofactor_value = cofactor_sign * determinant(submatrix)
             cofactor_row.append(cofactor_value)
