2e73c9c8bcf64f620a98cbfc63546cd43a80b33a
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 14:07:55 -0500

    definiteness

---
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""imports"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """determines the definiteness of a given square matrix"""
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+
+    if matrix.shape[0] != matrix.shape[1]:
+        return None
+
+    if not np.allclose(matrix, matrix.T):
+        return None
+
+    eigenvalues = np.linalg.eigvals(matrix)
+    all_positive = np.all(eigenvalues > 0)
+    all_nonnegative = np.all(eigenvalues >= 0)
+    all_negative = np.all(eigenvalues < 0)
+    all_nonpositive = np.all(eigenvalues <= 0)
+
+    if all_positive:
+        return "Positive definite"
+    elif all_nonnegative:
+        return "Positive semi-definite"
+    elif all_negative:
+        return "Negative definite"
+    elif all_nonpositive:
+        return "Negative semi-definite"
+    else:
+        return "Indefinite"
--- math/advanced_linear_algebra/5-main.py
+++ math/advanced_linear_algebra/5-main.py
@@ -1,16 +1,26 @@
 #!/usr/bin/env python3
 
-inverse = __import__('4-inverse').inverse
+if __name__ == '__main__':
+    definiteness = __import__('5-definiteness').definiteness
+    import numpy as np
 
-mat = [[10, 4, 7, 3, -9],
-       [-2, 8, 3, -5, 6],
-       [5, 19, 6, 1, 25],
-       [7, -30, 21, 4, -1],
-       [8, 9, -10, 2, -3]]
-print(inverse(mat))
-mat = [[5, 11, 6, 3, -20],
-       [1, -9, 13, 8, 5],
-       [2, 22, 4, 7, -6],
-       [-10, 3, 7, -1, 9],
-       [4, 8, -2, 10, 12]]
-print(inverse(mat))
+    mat1 = np.array([[5, 1], [1, 1]])
+    mat2 = np.array([[2, 4], [4, 8]])
+    mat3 = np.array([[-1, 1], [1, -1]])
+    mat4 = np.array([[-2, 4], [4, -9]])
+    mat5 = np.array([[1, 2], [2, 1]])
+    mat6 = np.array([])
+    mat7 = np.array([[1, 2, 3], [4, 5, 6]])
+    mat8 = [[1, 2], [1, 2]]
+
+    print(definiteness(mat1))
+    print(definiteness(mat2))
+    print(definiteness(mat3))
+    print(definiteness(mat4))
+    print(definiteness(mat5))
+    print(definiteness(mat6))
+    print(definiteness(mat7))
+    try:
+        definiteness(mat8)
+    except Exception as e:
+        print(e)
