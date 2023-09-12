acb2199b15576b86e18c6c7eaa52623d4021d13c
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 17:03:46 -0500

    syntax error

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -24,7 +24,7 @@ class MultiNormal:
         if not isinstance(x, np.ndarray):
             raise TypeError("x must be a numpy.ndarray")
         if x.shape != (self.mean.shape[0], 1):
-            raise ValueError(f"x must have the shape ({self.mean.shape[0]}, 1)")
+            raise ValueError("x must have the shape ({}, 1)".format(self.mean.shape[0]))
 
         d = self.mean.shape[0]
 
