54f701170530c654451de84aa7d281c59203d80d
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 11:46:14 -0500

    cofactor matrix

---
+++ math/advanced_linear_algebra/2-cofactor.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""cofactor matrix"""
+
+
+def cofactor(matrix):
+    """calculates the cofactor matrix of a given square matrix."""
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix must be a list of lists")
+
+    if not matrix or not matrix[0]:
+        raise ValueError("matrix must be a non-empty square matrix")
+
+
+    num_rows = len(matrix)
+    if not all(len(row) == num_rows for row in matrix):
+        raise ValueError("matrix must be a non-empty square matrix")
+
+    def determinant(submatrix):
+        if len(submatrix) == 2:
+            return submatrix[0][0] * submatrix[1][1] - submatrix[0][1] * submatrix[1][0]
+        det = 0
+        for i in range(len(submatrix)):
+            det += submatrix[0][i] * determinant([row[:i] + row[i + 1:] for row in submatrix[1:]])
+        return det
+
+    cofactor_matrix = []
+    for i in range(num_rows):
+        cofactor_row = []
+        for j in range(num_rows):
+            submatrix = [row[:j] + row[j + 1:] for row in (matrix[:i] + matrix[i + 1:])]
+            cofactor_sign = (-1) ** (i + j)
+            cofactor_value = cofactor_sign * determinant(submatrix)
+            cofactor_row.append(cofactor_value)
+        cofactor_matrix.append(cofactor_row)
+
+    return cofactor_matrix
