54f2086c4ea8c2ff8c36707240e75be9ee49d20d
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 16:33:35 -0500

    mean_conv

--- math/multivariate_prob/0-mean_cov.py
+++ math/multivariate_prob/0-mean_cov.py
@@ -12,14 +12,15 @@ def mean_cov(X):
 
     if n < 2:
         raise ValueError("X must contain multiple data points")
-    
+
     mean = np.mean(X, axis=0, keepdims=True)
 
     cov = np.zeros((d, d))
 
     for i in range(d):
         for j in range(i, d):
-            cov[i, j] = np.sum((X[:, i] - mean[:, i]) * (X[:, j] - mean[:, j])) / (n - 1)
+            cov[i, j] = np.sum((X[:, i] - mean[:, i]) *
+                               (X[:, j] - mean[:, j])) / (n - 1)
             cov[j, i] = cov[i, j]
 
     return mean, cov
