d61dc41192e0ae56260ff24f2a71f1fa129aabc6
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 11:07:20 -0500

    problems with minor matrix

--- math/advanced_linear_algebra/1-minor.py
+++ math/advanced_linear_algebra/1-minor.py
@@ -3,7 +3,7 @@
 
 
 def minor(matrix):
-    """calculates the minor marix of a matrix"""
+    """calculates the minor matrix of a matrix"""
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
---
+++ math/advanced_linear_algebra/2-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+minor = __import__('1-minor').minor
+
+mat = [[5]]
+print(minor(mat))
+mat = [[10]]
+print(minor(mat))
