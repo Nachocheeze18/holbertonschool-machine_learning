b6d9558a0eef6de9d758c432d7077ced10239d45
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-12 21:34:51 -0500

    syntax error

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -4,33 +4,34 @@ import numpy as np
 
 
 class MultiNormal:
-    """multinormal class"""
+    """Multivariate Normal distribution class"""
+
     def __init__(self, data):
-        """class constructor"""
+        """Class constructor"""
         if not isinstance(data, np.ndarray) or data.ndim != 2:
             raise TypeError("data must be a 2D numpy.ndarray")
-
+        
         n, d = data.shape
-        if d != 1:
-            raise ValueError("data must be univariate (1-dimensional)")
-
         if n < 2:
             raise ValueError("data must contain multiple data points")
 
-        self.mean = np.mean(data)
-        self.variance = np.var(data)
+        # Calculate the mean of the data
+        self.mean = np.mean(data, axis=1).reshape(-1, 1)
+
+        # Calculate the covariance matrix
+        mean_centered_data = data - self.mean
+        self.cov = (mean_centered_data @ mean_centered_data.T) / (n - 1)
 
     def pdf(self, x):
         """calculates the Probability Density Function"""
-        if not isinstance(x, np.ndarray) or x.ndim != 2:
-            raise TypeError("x must be a 2D numpy.ndarray")
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+        if x.shape != (self.mean.shape[0], 1):
+            raise ValueError("x must have the shape ({}, 1)".format(self.mean.shape[0]))
 
-        n, d = x.shape  # Get the number of data points and dimensionality
+        d = self.mean.shape[0]
 
-        if d != 1:
-            raise ValueError("x must be univariate (1-dimensional)")
-
-        exponent = -0.5 * ((x - self.mean) ** 2) / self.variance
-        denominator = np.sqrt(2 * np.pi * self.variance)
+        exponent = -0.5 * ((x - self.mean).T @ np.linalg.inv(self.cov) @ (x - self.mean))[0, 0]
+        denominator = (2 * np.pi) ** (-d / 2) * np.sqrt(np.linalg.det(self.cov))
         value = np.exp(exponent) / denominator
         return value
