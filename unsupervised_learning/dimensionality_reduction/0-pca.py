f235684c888d7827f3d87e353e61153d854e21c0
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-23 13:50:30 -0500

    peforms PCA on a dataset

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def pca(X, var=0.95):
     """Perform PCA on a dataset."""
-    _, S, Vt = np.linalg.svd(full_matrices=False)
+    _, S, Vt = np.linalg.svd(X, rowvar=False)
     total_variance = S**2 / np.sum(S**2)
     cumulative_variance = np.cumsum(total_variance)
     num_dimensions = np.argmax(cumulative_variance >= var) + 1
