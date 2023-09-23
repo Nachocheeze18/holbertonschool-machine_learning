1e2b4d999a15dd04e9f16745683a44eb531c9c75
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-23 15:54:10 -0500

    peforms PCA on a dataset

--- unsupervised_learning/dimensionality_reduction/1-pca.py
+++ unsupervised_learning/dimensionality_reduction/1-pca.py
@@ -5,8 +5,13 @@ import numpy as np
 
 def pca(X, ndim):
     """Perform PCA on a dataset with a specified number of dimensions."""
-    mean = X - np.mean(X, axis=0)
-    _, _, Vt = np.linalg.svd(mean, full_matrices=False)
-    W = Vt[:ndim].T
-    T = np.dot(mean, W)
+    mean = np.mean(X, axis=0)
+    centered_data = X - mean
+    cov_matrix = np.cov(centered_data, rowvar=False)
+    values, vectors = np.linalg.eigh(cov_matrix)
+    sorted_indices = np.argsort(values)[::-1]
+    values = values[sorted_indices]
+    vectors = vectors[:, sorted_indices]
+    top_vectors = vectors[:, :ndim]
+    T = np.dot(centered_data, top_vectors)
     return T
