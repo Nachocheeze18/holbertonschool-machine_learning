82a70508cae6d2aaee9b2da606cf1cd0388d4877
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-24 18:22:55 -0500

    optimum number of clusters by vairance

---
+++ unsupervised_learning/clustering/3-optimum.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Imports"""
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+variance = __import__('2-variance').variance
+
+
+def optimum_k(X, kmin=1, kmax=None, iterations=1000):
+    """ tests for the optimum number of clusters by variance"""
+    if type(X) is not np.ndarray or X.ndim != 2:
+        return None, None
+    if type(kmin) is not int or kmin < 1:
+        return None, None
+    if kmax is None:
+        kmax = X.shape[0]
+    if type(kmax) is not int or kmax <= 0 or kmin >= kmax:
+        return None, None
+    if type(iterations) is not int or iterations < 1:
+        return None, None
+
+
+    results = []
+    vars = []
+    d_vars = []
+
+    for k in range(kmin, kmax + 1):
+        cen, cluster = kmeans(X, k, iterations)
+        results.append((cen, cluster))
+        value = variance(X, cen)
+        vars.append(value)
+
+    ref = vars[0]
+    d_vars = [ref - var for var in vars]
+
+    return results, d_vars
