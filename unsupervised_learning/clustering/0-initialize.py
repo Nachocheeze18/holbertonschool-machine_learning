3c8c4a1bb34778aca2178c12c0a52690e330634c
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-23 21:17:34 -0500

    clustering

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -5,19 +5,17 @@ import numpy as np
 
 def initialize(X, k):
     """initializes cluster centroids for K-means"""
-    if not isinstance(X, np.ndarray) or X.ndim != 2:
+    try:
+        if k <= 0:
+            return None
+
+        n, d = X.shape
+
+        min_values = X.min(axis=0)
+        max_values = X.max(axis=0)
+
+        cen = np.random.uniform(min_values, max_values, (k, d))
+
+        return cen
+    except Exception:
         return None
-    if not isinstance(k, int) or k <= 0 or X.shape[0] < k:
-        return None
-
-    n, d = X.shape
-    cen = np.zeros((k, d))
-
-    cen[0] = X[np.random.choice(n, 1)]
-
-    for i in range(1, k):
-        dist = np.array([min(np.linalg.norm(x - c) ** 2 for c in cen[:i]) for x in X])
-        prob = dist / dist.sum()
-        cen[i] = X[np.random.choice(n, 1, p=prob)]
-
-    return cen
