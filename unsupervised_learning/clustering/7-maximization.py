5bde14a8f59fb1724a1fcdff9976fdcdb362e7bf
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-25 16:31:19 -0500

    calculates the maximization step in the EM algorithm for a GMM

--- unsupervised_learning/clustering/7-maximization.py
+++ unsupervised_learning/clustering/7-maximization.py
@@ -4,6 +4,7 @@ import numpy as np
 
 
 def maximization(X, g):
+    """calculates the maximization step in the EM algorithm for a GMM"""
     if not isinstance(X, np.ndarray) or len(X.shape) != 2:
         return None, None, None
     if not isinstance(g, np.ndarray) or len(g.shape) != 2:
