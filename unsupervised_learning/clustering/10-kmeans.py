66bbc4a95beaab60b58bcf49278b3feda655e94b
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-26 11:04:36 -0500

    peforms kmeans on data

--- unsupervised_learning/clustering/10-kmeans.py
+++ unsupervised_learning/clustering/10-kmeans.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 """Imports"""
-import sklearn.cluster 
+import sklearn.cluster
 
 
 def kmeans(X, k):
     """performs K-means on a dataset"""
     n, d = X.shape
 
-    kmeans = sklearn.cluster(n_clusters=k)
+    kmeans = sklearn.cluster.KMeans(n_clusters=k)  # Corrected the instantiation of KMeans
 
     kmeans.fit(X)
 
     C = kmeans.cluster_centers_
 
     clss = kmeans.labels_
-    
+
     return C, clss
