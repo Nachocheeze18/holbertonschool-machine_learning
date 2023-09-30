e8a617e2f21a84516185916a9cd9ef840c9454e1
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-29 21:55:42 -0500

    pycodestyle

--- unsupervised_learning/hmm/0-markov_chain.py
+++ unsupervised_learning/hmm/0-markov_chain.py
@@ -4,9 +4,16 @@ import numpy as np
 
 
 def markov_chain(P, s, t=1):
-    if (P.shape[0] != P.shape[1] or
-        s.shape[0] != 1 or P.shape[0]
-        != s.shape[1] or t < 1):
+    """
+    Determines the probability of a Markov chain
+    being in a particular state after a specified number of iterations
+    """
+    if (
+        P.shape[0] != P.shape[1]
+        or s.shape[0] != 1
+        or P.shape[0] != s.shape[1]
+        or t < 1
+    ):
         return None
 
     n = P.shape[0]
