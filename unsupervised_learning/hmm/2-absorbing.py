78f014a080d18fa4ee0470e3d6f5da7ee74220db
Nachocheeze18 <nachovelez490@gmail.com>
2023-09-30 13:34:25 -0500

    determines if a markov chain is absorbing

--- unsupervised_learning/hmm/2-absorbing.py
+++ unsupervised_learning/hmm/2-absorbing.py
@@ -2,13 +2,14 @@
 """Imports"""
 import numpy as np
 
+
 def absorbing(P):
     """determines if a markov chain is absorbing"""
     if not isinstance(P, np.ndarray):
         return False
-    
+
     n = P.shape[0]
-    
+
     if P.shape != (n, n):
         return False
 
@@ -16,10 +17,10 @@ def absorbing(P):
     for i in range(n):
         if P[i, i] == 1:
             absorbing_states.add(i)
-    
+
     if not absorbing_states:
         return False
-    
+
     for i in absorbing_states:
         for j in range(n):
             if j not in absorbing_states and P[i, j] != 0:
