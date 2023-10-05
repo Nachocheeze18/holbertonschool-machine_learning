a4fa7b6decc97e23638ad34df26b30de170f0f26
Nachocheeze18 <5581@holbertonstudents.com>
2023-10-05 13:35:57 -0500

    baumwelch

---
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Imports"""
+
+
+def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
+    """ performs the Baum-Welch algorithm for a hidden markov model"""
+    return "Hello World"
