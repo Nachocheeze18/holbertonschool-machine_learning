d87c1d510f6427da5b5efa5b3bb4aaee23c32d8d
Nachocheeze18 <5581@holbertonstudents.com>
2023-10-22 13:17:10 -0500

    creates a convolutional autoencoder

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -11,8 +11,9 @@ def autoencoder(input_dims, filters, latent_dims):
     for filter_size in filters:
         x = keras.layers.Conv2D(filter_size, (3, 3), activation='relu', padding='same')(x)
         x = keras.layers.MaxPooling2D((2, 2))(x)
-
-    encoder = keras.models.Model(input_layer, x, name='encoder')
+    latent = keras.layers.Conv2D(latent_dims[-1], (3, 3),
+                                 activation='relu', padding='same')(x)
+    encoder = keras.models.Model(input_layer, latent)
 
     latent_input = keras.layers.Input(shape=latent_dims)
     x = latent_input
@@ -21,7 +22,6 @@ def autoencoder(input_dims, filters, latent_dims):
         x = keras.layers.Conv2D(filter_size, (3, 3), activation='relu', padding='same')(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
 
-    # Adjust the padding of the last MaxPooling2D layer to 'valid'
     x = keras.layers.Conv2D(filters[-1], (3, 3), activation='relu', padding='valid')(x)
     x = keras.layers.UpSampling2D((2, 2))(x)
 
