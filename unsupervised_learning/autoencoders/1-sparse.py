035a0153e0dfd906ac01bcee49a938d307f1dd06
Nachocheeze18 <5581@holbertonstudents.com>
2023-10-22 12:08:30 -0500

    creates a sparse autoencoder

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -7,27 +7,33 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
     """Creates an autoencoder or a sparse autoencoder depending on lambtha value"""
     encoder_input = keras.Input(shape=(input_dims,))
     x = encoder_input
-
     for nodes in hidden_layers:
         x = keras.layers.Dense(nodes, activation='relu')(x)
-    latent = keras.layers.Dense(latent_dims, activation='relu')(x)
+    latent = keras.layers.Dense(latent_dims, activation='relu', activity_regularizer=keras.regularizers.l1(lambtha))(x)
     encoder = keras.Model(encoder_input, latent)
+
+    # Decoder
     decoder_input = keras.Input(shape=(latent_dims,))
     x = decoder_input
-
     for nodes in reversed(hidden_layers):
         x = keras.layers.Dense(nodes, activation='relu')(x)
     output = keras.layers.Dense(input_dims, activation='sigmoid')(x)
     decoder = keras.Model(decoder_input, output)
 
+    # Autoencoder
     autoencoder_input = keras.Input(shape=(input_dims,))
     encoded = encoder(autoencoder_input)
     decoded = decoder(encoded)
     auto = keras.Model(autoencoder_input, decoded)
 
-    l1_regularizer = lambtha * keras.backend.abs(encoded)
-    auto.add_loss(l1_regularizer)
-
+    # Compile autoencoder
     auto.compile(optimizer='adam', loss='binary_crossentropy')
+
+    # Print to verify the encoder
+    print(encoder.layers[0].get_config()['name'])
+    for layer in encoder.layers[1:]:
+        print(layer.__class__.__name__)
+        print(layer.activation)
+        print(layer.units)
 
     return encoder, decoder, auto
