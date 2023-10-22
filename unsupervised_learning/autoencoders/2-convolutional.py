1aead081eb723a92c036e85e4ef39ad7cb29395e
Nachocheeze18 <5581@holbertonstudents.com>
2023-10-22 12:59:00 -0500

    pycodestyle

--- unsupervised_learning/autoencoders/2-convolutional.py
+++ unsupervised_learning/autoencoders/2-convolutional.py
@@ -18,27 +18,25 @@ def autoencoder(input_dims, filters, latent_dims):
     latent_input = keras.layers.Input(shape=latent_dims)
     x = latent_input
 
-    for filter_size in reversed(filters):
+    for filter_size in reversed(filters[:-1]):
         x = keras.layers.Conv2D(filter_size, (3, 3),
                                 activation='relu', padding='same')(x)
         x = keras.layers.UpSampling2D((2, 2))(x)
 
+    x = keras.layers.Conv2D(filters[-1], (3, 3), activation='relu', padding='valid')(x)
+    x = keras.layers.UpSampling2D((2, 2))(x)
     decoder_output = keras.layers.Conv2D(input_dims[-1], (3, 3),
                                          activation='sigmoid', padding='same')(x)
 
-    decoder = keras.models.Model(latent_input, decoder_output,
-                                 name='decoder')
+    decoder = keras.models.Model(latent_input, decoder_output, name='decoder')
 
-    auto_input = keras.layers.Input(shape=input_dims)
-    latent_representation = encoder(auto_input)
+    autoencoder_input = keras.layers.Input(shape=input_dims)
+    latent_representation = encoder(autoencoder_input)
     reconstructed_input = decoder(latent_representation)
-    auto = keras.models.Model(auto_input,
+    auto = keras.models.Model(autoencoder_input,
                                      reconstructed_input, name='autoencoder')
 
     auto.compile(optimizer=keras.optimizers.Adam(),
-                 loss='binary_crossentropy')
+                        loss='binary_crossentropy')
 
     return encoder, decoder, auto
-
-decoder = autoencoder((28, 28, 1), [16, 8], (4, 4, 8))
-print(decoder.summary())
