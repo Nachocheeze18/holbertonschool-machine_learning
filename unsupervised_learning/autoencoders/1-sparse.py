2c8839295ba7435d469fff7d3bba7cf9370f71f4
Nachocheeze18 <5581@holbertonstudents.com>
2023-10-22 12:17:29 -0500

    creates a sparse autoencoder

--- unsupervised_learning/autoencoders/1-sparse.py
+++ unsupervised_learning/autoencoders/1-sparse.py
@@ -31,8 +31,7 @@ def autoencoder(input_dims, hidden_layers, latent_dims, lambtha):
 
     # Check conditions for encoder layers
     conditions = ([layer.activation == keras.activations.relu and
-                   layer.activation is not None and layer.units
-                   is not None for layer in encoder.layers[1:]])
+                   layer.units is not None for layer in encoder.layers[1:]])
     print(all(conditions))
 
     return encoder, decoder, auto
