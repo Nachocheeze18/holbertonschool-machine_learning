1eeb00ee69f8db779cf13dcf02f20b5df81e6534
Nachocheeze18 <5581@holbertonstudents.com>
2023-11-08 11:29:10 -0600

    rpersents the cell of a simple rnn

---
+++ supervised_learning/RNNs/0-rnn_cell.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Imports"""
+import numpy as np
+
+
+class RNNCell:
+    """RNN Class"""
+    def __init__(self, i, h, o):
+        """class constructor"""
+        self.i_dim = i
+        self.h_dim = h
+        self.o_dim = o
+        self.Wh = np.random.randn(i + h, h)
+        self.Wy = np.random.randn(h, o)
+        self.bh = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def forward(self, h_prev, x_t):
+        """previous hidden state and input data"""
+        concat = np.hstack((h_prev, x_t))
+        h_next = np.tanh(np.dot(concat, self.Wh) + self.bh)
+        y = np.dot(h_next, self.Wy) + self.by
+
+        return h_next, y
