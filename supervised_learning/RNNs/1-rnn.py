3493b42f9018a6830b3604985768686a6e79f825
Nachocheeze18 <5581@holbertonstudents.com>
2023-11-08 13:27:54 -0600

    forward prop

--- supervised_learning/RNNs/1-rnn.py
+++ supervised_learning/RNNs/1-rnn.py
@@ -5,16 +5,21 @@ import numpy as np
 
 def rnn(rnn_cell, X, h_0):
     """performs forward propagation for a simple RNN"""
-    t, m, i = X.shape
+    t, m, _ = X.shape
     h = h_0.shape[1]
+
     H = np.zeros((t + 1, m, h))
-    Y = np.zeros((t, m, rnn_cell.Wy.shape[1]))
+    H[0] = h_0
+
+    o = rnn_cell.Wy.shape[1]
+    Y = np.zeros((t, m, o))
+
     h_t = h_0
 
     for step in range(t):
         x_t = X[step, :, :]
         h_t, y_t = rnn_cell.forward(h_t, x_t)
-        H[step, :, :] = h_t
+        H[step + 1, :, :] = h_t
         Y[step, :, :] = y_t
 
     return H, Y
