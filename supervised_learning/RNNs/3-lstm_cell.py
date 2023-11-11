46381ecd3c65650d5428137079115ab7cf07dceb
Nachocheeze18 <5581@holbertonstudents.com>
2023-11-10 20:41:04 -0600

    represents an LSTM unit

--- supervised_learning/RNNs/3-lstm_cell.py
+++ supervised_learning/RNNs/3-lstm_cell.py
@@ -18,30 +18,33 @@ class LSTMCell:
         self.Wy = np.random.randn(h, o)
         self.by = np.zeros((1, o))
 
+
+    def forward(self, h_prev, c_prev, x_t):
+        """Performs forward propagation for one time step."""
+        # Concatenate previous hidden state and input data
+        concat = np.concatenate((h_prev, x_t), axis=1)
+
+        f = sigmoid(np.dot(concat, self.Wf) + self.bf)
+
+        u = sigmoid(np.dot(concat, self.Wu) + self.bu)
+
+        c_hat = np.tanh(np.dot(concat, self.Wc) + self.bc)
+
+        c_next = f * c_prev + u * c_hat
+
+        o = sigmoid(np.dot(concat_input, self.Wo) + self.bo)
+
+        h_next = o * np.tanh(c_next)
+
+        y = softmax(np.dot(h_next, self.Wy) + self.by)
+
+        return h_next, c_next, y
+
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 def softmax(x):
     exp_x = np.exp(x - np.max(x, axis=1, keepdims=True))
     return exp_x / np.sum(exp_x, axis=1, keepdims=True)
-
-def forward(self, h_prev, c_prev, x_t):
-    """Performs forward propagation for one time step."""
-    concat = np.concatenate((h_prev, x_t), axis=1)
-
-    f = sigmoid(np.dot(concat, self.Wf) + self.bf)
-
-    u = sigmoid(np.dot(concat, self.Wu) + self.bu)
-
-    c_hat = np.tanh(np.dot(concat, self.Wc) + self.bc)
-
-    c_next = f * c_prev + u * c_hat
-
-    o = sigmoid(np.dot(concat, self.Wo) + self.bo)
-
-    h_next = o * np.tanh(c_next)
-
-    y = softmax(np.dot(h_next, self.Wy) + self.by)
-
-    return h_next, c_next, y
-
