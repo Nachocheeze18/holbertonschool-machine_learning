2de68845f33a18dcbc4f02eea061cd7f5aca1e33
Nachocheeze18 <5581@holbertonstudents.com>
2023-11-30 10:19:12 -0600

    embedding matrix

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Imports"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+
+def tf_idf(sentences, vocab=None):
+    if vocab == None:
+        vocab = set(word for sentence in sentences for word in sentence.split())
+
+    vec = TfidfVectorizer(vocabulary=vocab)
+
+    embeddings = vec.fit_transform(sentences).toarray()
+
+    features = vec.get_feature_names_out()
+
+    z = embeddings, features
+    return z
