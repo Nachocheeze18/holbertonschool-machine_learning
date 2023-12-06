c436509b91141e0e837ad7512526338a6eed399c
Nachocheeze18 <5581@holbertonstudents.com>
2023-12-06 15:22:15 -0600

    calculates the unigram BLEU score for a sentence

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -5,14 +5,11 @@ import numpy as np
 
 def uni_bleu(references, sentence):
     """Convert references to the required format"""
-    word_counts = {word: min([ref.count(word) for ref in references], default=0) for word in sentence}
-    total_matches = sum(word_counts.values())
+    total_word_count_refs = sum(len(ref) for ref in references)
+    total_matches = sum(min(ref.count(word) for ref in references) for word in sentence)
     precision = total_matches / len(sentence) if len(sentence) > 0 else 0
 
-    reference_length = min(len(ref) for ref in references)
-    brevity_penalty = np.exp(1 - (reference_length / len(sentence))) if len(sentence) < reference_length else 1
-
-    brevity_penalty = 1
+    brevity_penalty = np.exp(1 - total_word_count_refs / len(sentence)) if len(sentence) < total_word_count_refs else 1
 
     bleu_score = brevity_penalty * precision
 
