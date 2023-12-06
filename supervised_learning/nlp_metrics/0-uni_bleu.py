d94540e8ebc2c02fb636549b20fd77677669631f
Nachocheeze18 <5581@holbertonstudents.com>
2023-12-06 15:10:30 -0600

    calculates the unigram BLEU score for a sentence

--- supervised_learning/nlp_metrics/0-uni_bleu.py
+++ supervised_learning/nlp_metrics/0-uni_bleu.py
@@ -8,18 +8,23 @@ def uni_bleu(references, sentence):
     ref_list = [[ref] for ref in references]
 
     max_len = max(len(ref) for ref in ref_list)
-    ref_list = [ref + [None] * (max_len - len(ref)) for ref in ref_list]
-    sentence = sentence + [None] * (max_len - len(sentence))
+    ref_list = [ref + [None] * (
+        max_len - len(ref)) for ref in ref_list]
+    sentence = sentence + [None] * (
+        max_len - len(sentence))
 
-    precision = 0
+    match_counts = 0
+    total_counts = 0
     for w in sentence:
-        match_counts = [
-            np.count_nonzero(np.array(ref) == w) for ref in ref_list
-        ]
-        precision += np.max(match_counts) / len(sentence)
+        if w is not None:
+            match_counts += max(
+                [ref.count(w) for ref in ref_list])
+            total_counts += 1
 
-    bp = 1 if len(sentence) > len(ref_list[0][0]) else np.exp(1 - len(ref_list[0][0]) / len(sentence))
-    
+    precision = match_counts / total_counts
+
+    bp = min(1, len(sentence) / len(ref_list[0][0]))
+
     score = bp * np.exp(np.log(precision))
 
     return score
