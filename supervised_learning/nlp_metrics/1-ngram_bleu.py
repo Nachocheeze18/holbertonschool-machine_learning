1c26358261a84438441a2594cf3b4f213a26fa9d
Nachocheeze18 <5581@holbertonstudents.com>
2023-12-06 21:28:33 -0600

    n gram bleu score

--- supervised_learning/nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/nlp_metrics/1-ngram_bleu.py
@@ -1,30 +1,52 @@
 #!/usr/bin/env python3
 """Imports"""
-import numpy as np
-
+from math import exp, log
+from collections import Counter
 
 def ngram_bleu(references, sentence, n):
-    """calculates the n-gram BLEU score for a sentence"""
-    reference_lengths = np.array([len(ref) for ref in references])
-    sentence_length = len(sentence)
+    """
+    Calculate the n-gram BLEU score for a sentence.
 
-    precisions = []
-    for i in range(1, n + 1):
-        reference_ngrams = np.concatenate([np.array([tuple(ref[j:j + i]) for j in range(len(ref) - i + 1)]) for ref in references])
-        sentence_ngrams = np.array([tuple(sentence[j:j + i]) for j in range(sentence_length - i + 1)])
+    Args:
+    references (list): List of reference translations.
+    sentence (list): List containing the model proposed sentence.
+    n (int): Size of the n-gram to use for evaluation.
 
-        overlaps = np.minimum(
-            np.histogram(reference_ngrams, bins=np.unique(np.concatenate([reference_ngrams, sentence_ngrams])))[0],
-            np.histogram(sentence_ngrams, bins=np.unique(np.concatenate([reference_ngrams, sentence_ngrams])))[0]
-        )
+    Returns:
+    float: The n-gram BLEU score.
+    """
 
-        precision = overlaps.sum() / len(sentence_ngrams)
-        precisions.append(precision)
+    def calculate_precision(candidate, reference, n):
+        candidate_ngrams = [tuple(candidate[i:i + n]) for i in range(len(candidate) - n + 1)]
+        reference_ngrams = [tuple(reference[i:i + n]) for i in range(len(reference) - n + 1)]
 
-    geo_mean = np.exp(np.mean(np.log(precisions)))
+        candidate_ngram_counts = Counter(candidate_ngrams)
+        reference_ngram_counts = Counter(reference_ngrams)
 
-    brevity_penalty = np.exp(1 - reference_lengths / sentence_length) if sentence_length < np.min(reference_lengths) else 1.0
+        clipped_counts = {ngram: min(candidate_ngram_counts[ngram], reference_ngram_counts[ngram]) for ngram in candidate_ngram_counts}
 
-    bleu_score = brevity_penalty * geo_mean
+        total_clipped_counts = sum(clipped_counts.values())
+        total_candidate_counts = sum(candidate_ngram_counts.values())
+
+        precision = total_clipped_counts / total_candidate_counts if total_candidate_counts > 0 else 0.0
+
+        return precision
+
+    def brevity_penalty(candidate, references):
+        candidate_length = len(candidate)
+        reference_lengths = [len(reference) for reference in references]
+        closest_length = min(reference_lengths, key=lambda x: abs(x - candidate_length))
+        brevity_penalty = 1.0 if candidate_length >= closest_length else exp(1 - closest_length / candidate_length)
+
+        return brevity_penalty
+
+    precision_scores = []
+
+    for i in range(n):
+        precision_i = sum(calculate_precision(sentence, reference, i + 1) for reference in references) / len(references)
+        precision_scores.append(precision_i)
+
+    geometric_mean = (precision_i ** (1 / n) for precision_i in precision_scores)
+    bleu_score = brevity_penalty(sentence, references) * exp(sum(log(precision_i) for precision_i in geometric_mean) / n)
 
     return bleu_score
