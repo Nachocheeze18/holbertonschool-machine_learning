75fcfe9e83cec18c1701c0a7a2d7b23ee295ff1c
Nachocheeze18 <5581@holbertonstudents.com>
2023-12-06 22:25:10 -0600

    n gram bleu score

--- supervised_learning/nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/nlp_metrics/2-cumulative_bleu.py
@@ -1,37 +1,43 @@
 #!/usr/bin/env python3
 """Imports"""
-from collections import Counter
+import numpy as np
 
 
-def cumulative_bleu(references, sentence, n):
-    """Calculate cumulative n-gram BLEU score for a sentence."""
+def cumulative_bleu(references, sentence, N):
+    """Returns: the cumulative bleu score"""
+    def make_ngrams(sentence, n):
+        """converts a sentence to ngrams"""
+        ngrams = [' '.join(sentence[i:i+n]) for i in range(len(sentence) - n + 1)]
+        return ngrams
 
-    def calculate_precision(candidate, references, n):
-        """Calculate precision for a given n-gram."""
-        candidate_ngrams = Counter(zip(*[candidate[i:] for i in range(n)]))
-        mrn = Counter()
+    def count_ngrams(sentence, ngrams):
+        ngram_count = {ngram: 0 for ngram in ngrams}
+        for ngram in make_ngrams(sentence, len(ngrams[0].split())):
+            if ngram in ngrams:
+                ngram_count[ngram] += 1
 
-        for ref in references:
-            reference_ngrams = Counter(zip(*[ref[i:] for i in range(n)]))
-            mrn += reference_ngrams
+        return ngram_count
 
-        clipped_ngrams = {ngram: min(candidate_ngrams[ngram], mrn[ngram]) for ngram in candidate_ngrams}
+    Pn = []
+    for n in range(1, N + 1):
+        ngrams = make_ngrams(sentence, n)
 
-        precision = sum(clipped_ngrams.values()) / max(1, sum(candidate_ngrams.values()))
+        c = len(sentence)
+        r = min(len(reference) for reference in references)
+        BP = 1 if c > r else np.exp(1 - (r / c))
 
-        return precision
+        max_ref_count = {ngram: 0 for ngram in ngrams}
+        for reference in references:
+            ngram_count = count_ngrams(reference, ngrams)
+            for ngram in ngrams:
+                max_ref_count[ngram] = max(ngram_count[ngram], max_ref_count[ngram])
 
-    bleu = 1.0
+        precision = np.sum(list(max_ref_count.values())) / np.sum(list(count_ngrams(sentence, ngrams).values()))
+        Pn.append(precision)
 
-    for i in range(1, n + 1):
-        precision_i = calculate_precision(sentence, references, i)
-        bleu *= precision_i
+    if 0 in Pn:
+        Pn.remove(0)
 
-    reference_length = min(len(ref) for ref in references)
-    candidate_length = len(sentence)
-    
-    brevity_penalty = min(1, reference_length / candidate_length)
-
-    bleu = brevity_penalty * bleu ** (1/n)
+    bleu = BP * np.exp(np.sum((1/N) * np.log(Pn)))
 
     return bleu
